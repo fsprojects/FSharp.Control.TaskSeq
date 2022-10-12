15e9fcc8cff4a7a75f4d0cd9ea821e51e946d597
Abel Braaksma <abel.online@xs4all.nl>
2022-10-12 02:44:50 +0200

    Initial taskSeq implementation with some modifications of the original source

---
+++ src/FSharpy.TaskSeq/TaskSeqInternal.fs
@@ -0,0 +1,101 @@
+﻿namespace FSharpy.TaskSeq
+
+open System.Collections.Generic
+open System.Threading
+open System.Threading.Tasks
+
+module TaskSeq =
+    module internal Internal =
+        let iteriAsync action (taskSeq: taskSeq<_>) = task {
+            let e = taskSeq.GetAsyncEnumerator(CancellationToken())
+            let mutable go = true
+            let mutable i = 0
+            let! step = e.MoveNextAsync()
+            go <- step
+
+            while go do
+                action i e.Current
+                let! step = e.MoveNextAsync()
+                i <- i + 1
+                go <- step
+        }
+
+        let fold (action: 'State -> 'T -> 'State) initial (taskSeq: taskSeq<_>) = task {
+            let e = taskSeq.GetAsyncEnumerator(CancellationToken())
+            let mutable go = true
+            let mutable result = initial
+            let! step = e.MoveNextAsync()
+            go <- step
+
+            while go do
+                result <- action result e.Current
+                let! step = e.MoveNextAsync()
+                go <- step
+
+            return result
+        }
+
+        let toResizeArrayAsync taskSeq = task {
+            let res = ResizeArray()
+            do! taskSeq |> iteriAsync (fun _ item -> res.Add item)
+            return res
+        }
+
+        let mapi mapper (taskSequence: taskSeq<_>) = taskSeq {
+            let mutable i = 0
+
+            for c in taskSequence do
+                yield mapper i c
+                i <- i + 1
+        }
+
+        let mapiAsync (mapper: _ -> _ -> Task<'T>) (taskSequence: taskSeq<_>) = taskSeq {
+            let mutable i = 0
+
+            for c in taskSequence do
+                let! x = mapper i c
+                yield x
+                i <- i + 1
+        }
+
+        let zip (taskSequence1: taskSeq<_>) (taskSequence2: taskSeq<_>) = taskSeq {
+            let e1 = taskSequence1.GetAsyncEnumerator(CancellationToken())
+            let e2 = taskSequence2.GetAsyncEnumerator(CancellationToken())
+            let mutable go = true
+            let! step1 = e1.MoveNextAsync()
+            let! step2 = e1.MoveNextAsync()
+            go <- step1 && step2
+
+            while go do
+                yield e1.Current, e2.Current
+                let! step1 = e1.MoveNextAsync()
+                let! step2 = e1.MoveNextAsync()
+                go <- step1 && step2
+
+            if step1 then
+                invalidArg "taskSequence1" "The task sequences had different lengths."
+
+            if step2 then
+                invalidArg "taskSequence2" "The task sequences had different lengths."
+        }
+
+        let collect (binder: _ -> #IAsyncEnumerable<_>) (taskSequence: taskSeq<_>) = taskSeq {
+            for c in taskSequence do
+                yield! binder c :> IAsyncEnumerable<_>
+        }
+
+        let collectSeq (binder: _ -> #seq<_>) (taskSequence: taskSeq<_>) = taskSeq {
+            for c in taskSequence do
+                yield! binder c :> seq<_>
+        }
+
+    /// Returns taskSeq as an array. This function is blocking until the sequence is exhausted.
+    let toListResult (t: taskSeq<'T>) = [
+        let e = t.GetAsyncEnumerator(CancellationToken())
+
+        try
+            while (let vt = e.MoveNextAsync() in if vt.IsCompleted then vt.Result else vt.AsTask().Result) do
+                yield e.Current
+        finally
+            e.DisposeAsync().AsTask().Wait()
+    ]
---
+++ src/FSharpy.TaskSeq/Utils.fs
@@ -0,0 +1,63 @@
+﻿namespace FSharpy.TaskSeq
+
+open System.Threading.Tasks
+
+module Task =
+    /// Convert an Async<'T> into a Task<'T>
+    let inline ofAsync (async: Async<'T>) = task { return! async }
+
+    /// Convert a unit-task into a Task<unit>
+    let inline ofTask (task': Task) = task { do! task' }
+
+    /// Convert a Task<'T> into an Async<'T>
+    let inline toAsync (task: Task<'T>) = Async.AwaitTask task
+
+    /// Convert a Task<unit> into a Task
+    let inline toTask (task: Task<unit>) = task :> Task
+
+    /// Convert a Task<'T> into a Task, ignoring the result
+    let inline ignore (task: Task<'T>) =
+        TaskBuilder.task {
+            let! _ = task
+            return ()
+        }
+        :> Task
+
+    /// Map a Tas<'T>
+    let inline map mapper (task: Task<'T>) : Task<'U> =
+        TaskBuilder.task {
+            let! result = task
+            return mapper result
+        }
+
+    /// Bind a Task<'T>
+    let inline bind binder (task: Task<'T>) : Task<'U> =
+        TaskBuilder.task { return! binder task }
+
+module Async =
+    /// Convert an Task<'T> into an Async<'T>
+    let inline ofTask (task: Task<'T>) = Async.AwaitTask task
+
+    /// Convert a unit-task into an Async<unit>
+    let inline ofUnitTask (task: Task) = Async.AwaitTask task
+
+    /// Convert a Task<'T> into an Async<'T>
+    let inline toTask (async: Async<'T>) = task { return! async }
+
+    /// Convert an Async<'T> into an Async<unit>, ignoring the result
+    let inline ignore (async': Async<'T>) =
+        async {
+            let! _ = async'
+            return ()
+        }
+
+    /// Map an Async<'T>
+    let inline map mapper (async: Async<'T>) : Async<'U> =
+        ExtraTopLevelOperators.async {
+            let! result = async
+            return mapper result
+        }
+
+    /// Bind an Async<'T>
+    let inline bind binder (task: Async<'T>) : Async<'U> =
+        ExtraTopLevelOperators.async { return! binder task }
