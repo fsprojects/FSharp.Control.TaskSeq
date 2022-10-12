06662728da9aa2c07ca78778543039508f479976
Abel Braaksma <abel.online@xs4all.nl>
2022-10-12 18:51:56 +0200

    Fix tests

--- src/FSharpy.TaskSeq.Test/TaskSeq.ToXXX.Tests.fs
+++ src/FSharpy.TaskSeq.Test/TaskSeq.ToXXX.Tests.fs
@@ -24,51 +24,51 @@ open System.Collections.Generic
 let ``TaskSeq-toArrayAsync should succeed`` () = task {
     let tq = createDummyTaskSeq 10
     let! (results: _[]) = tq |> TaskSeq.toArrayAsync
-    results |> should equal [| 0..9 |]
+    results |> should equal [| 1..10 |]
 }
 
 [<Fact>]
 let ``TaskSeq-toListAsync should succeed`` () = task {
     let tq = createDummyTaskSeq 10
     let! (results: list<_>) = tq |> TaskSeq.toListAsync
-    results |> should equal [ 0..9 ]
+    results |> should equal [ 1..10 ]
 }
 
 [<Fact>]
 let ``TaskSeq-toSeqCachedAsync should succeed`` () = task {
     let tq = createDummyTaskSeq 10
     let! (results: seq<_>) = tq |> TaskSeq.toSeqCachedAsync
-    results |> Seq.toArray |> should equal [| 0..9 |]
+    results |> Seq.toArray |> should equal [| 1..10 |]
 }
 
 [<Fact>]
 let ``TaskSeq-toIListAsync should succeed`` () = task {
     let tq = createDummyTaskSeq 10
     let! (results: IList<_>) = tq |> TaskSeq.toIListAsync
-    results |> Seq.toArray |> should equal [| 0..9 |]
+    results |> Seq.toArray |> should equal [| 1..10 |]
 }
 
 [<Fact>]
 let ``TaskSeq-toResizeArray should succeed`` () = task {
     let tq = createDummyTaskSeq 10
     let! (results: ResizeArray<_>) = tq |> TaskSeq.toResizeArrayAsync
-    results |> should equal [| 0..9 |]
+    results |> Seq.toArray |> should equal [| 1..10 |]
 }
 
 [<Fact>]
 let ``TaskSeq-toArray should succeed and be blocking`` () =
     let tq = createDummyTaskSeq 10
     let (results: _[]) = tq |> TaskSeq.toArray
-    results |> should equal [| 0..9 |]
+    results |> should equal [| 1..10 |]
 
 [<Fact>]
 let ``TaskSeq-toList should succeed and be blocking`` () =
     let tq = createDummyTaskSeq 10
     let (results: list<_>) = tq |> TaskSeq.toList
-    results |> should equal [ 0..9 ]
+    results |> should equal [ 1..10 ]
 
 [<Fact>]
 let ``TaskSeq-toSeqCached should succeed and be blocking`` () =
     let tq = createDummyTaskSeq 10
     let (results: seq<_>) = tq |> TaskSeq.toSeqCached
-    results |> Seq.toArray |> should equal [| 0..9 |]
+    results |> Seq.toArray |> should equal [| 1..10 |]
