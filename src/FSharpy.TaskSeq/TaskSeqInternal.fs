e6f49b10d3e52d445a67add7e2b8c072c8a80010
Abel Braaksma <abel.online@xs4all.nl>
2022-10-13 22:22:07 +0200

    Add internals for TaskSeq.tryItem/Last/Head, tryFind/Pick, choose/filter

--- src/FSharpy.TaskSeq/TaskSeqInternal.fs
+++ src/FSharpy.TaskSeq/TaskSeqInternal.fs
@@ -1,5 +1,6 @@
 namespace FSharpy
 
+open System
 open System.Collections.Generic
 open System.Threading
 open System.Threading.Tasks
@@ -22,7 +23,26 @@ type FolderAction<'T, 'State, 'TaskState when 'TaskState :> Task<'State>> =
     | FolderAction of state_action: ('State -> 'T -> 'State)
     | AsyncFolderAction of async_state_action: ('State -> 'T -> 'TaskState)
 
+[<Struct>]
+type ChooserAction<'T, 'U, 'TaskOption, 'TaskBool when 'TaskOption :> Task<'U option> and 'TaskBool :> Task<bool>> =
+    | TryPick of try_pick: ('T -> 'U option)
+    | TryPickAsync of async_try_pick: ('T -> 'TaskOption)
+    | TryFilter of try_filter: ('T -> bool)
+    | TryFilterAsync of async_try_filter: ('T -> 'TaskBool)
+
 module internal TaskSeqInternal =
+    let inline raiseEmptySeq () =
+        ArgumentException("The asynchronous input sequence was empty.", "taskSeq")
+        |> raise
+
+    let inline raiseInsufficient () =
+        ArgumentException("The asynchronous input sequence was has an insufficient number of elements.", "taskSeq")
+        |> raise
+
+    let inline raiseNotFound () =
+        KeyNotFoundException("The predicate function or index did not satisfy any item in the async sequence.")
+        |> raise
+
     let iter action (taskSeq: taskSeq<_>) = task {
         let e = taskSeq.GetAsyncEnumerator(CancellationToken())
         let mutable go = true
@@ -141,10 +161,10 @@ module internal TaskSeqInternal =
             go <- step1 && step2
 
         if step1 then
-            invalidArg "taskSequence1" "The task sequences had different lengths."
+            invalidArg "taskSequence1" "The task sequences have different lengths."
 
         if step2 then
-            invalidArg "taskSequence2" "The task sequences had different lengths."
+            invalidArg "taskSequence2" "The task sequences have different lengths."
     }
 
     let collect (binder: _ -> #IAsyncEnumerable<_>) (taskSequence: taskSeq<_>) = taskSeq {
@@ -169,13 +189,131 @@ module internal TaskSeqInternal =
             yield! result :> seq<_>
     }
 
-    /// Returns taskSeq as an array. This function is blocking until the sequence is exhausted.
-    let toListResult (t: taskSeq<'T>) = [
-        let e = t.GetAsyncEnumerator(CancellationToken())
+    let tryLast (taskSeq: taskSeq<_>) = task {
+        let e = taskSeq.GetAsyncEnumerator(CancellationToken())
+        let mutable go = true
+        let mutable last = ValueNone
+        let! step = e.MoveNextAsync()
+        go <- step
 
-        try
-            while (let vt = e.MoveNextAsync() in if vt.IsCompleted then vt.Result else vt.AsTask().Result) do
-                yield e.Current
-        finally
-            e.DisposeAsync().AsTask().Wait()
-    ]
+        while go do
+            last <- ValueSome e.Current
+            let! step = e.MoveNextAsync()
+            go <- step
+
+        match last with
+        | ValueSome value -> return Some value
+        | ValueNone -> return None
+    }
+
+    let tryHead (taskSeq: taskSeq<_>) = task {
+        let e = taskSeq.GetAsyncEnumerator(CancellationToken())
+        let mutable go = true
+        let! step = e.MoveNextAsync()
+        go <- step
+
+        if go then return Some e.Current else return None
+    }
+
+    let tryItem i (taskSeq: taskSeq<_>) = task {
+        let e = taskSeq.GetAsyncEnumerator(CancellationToken())
+        let mutable go = true
+        let mutable idx = 0
+        let mutable foundItem = None
+        let! step = e.MoveNextAsync()
+        go <- step
+
+        while go && idx <= i do
+            if idx = i then
+                foundItem <- Some e.Current
+
+            let! step = e.MoveNextAsync()
+            go <- step
+            idx <- idx + 1
+
+        return foundItem
+    }
+
+    /// Supports all four types of picking: pick/find/pick-async/find-async
+    let tryPick chooser (taskSeq: taskSeq<_>) = task {
+        let e = taskSeq.GetAsyncEnumerator(CancellationToken())
+
+        let mutable go = true
+        let mutable foundItem = None
+        let! step = e.MoveNextAsync()
+        go <- step
+
+        match chooser with
+        | TryPick picker ->
+            while go do
+                match picker e.Current with
+                | Some value ->
+                    foundItem <- Some value
+                    go <- false
+                | None ->
+                    let! step = e.MoveNextAsync()
+                    go <- step
+
+        | TryPickAsync picker ->
+            while go do
+                match! picker e.Current with
+                | Some value ->
+                    foundItem <- Some value
+                    go <- false
+                | None ->
+                    let! step = e.MoveNextAsync()
+                    go <- step
+
+        | TryFilter filterer ->
+            while go do
+                let current = e.Current
+
+                match filterer current with
+                | true ->
+                    foundItem <- Some current
+                    go <- false
+                | false ->
+                    let! step = e.MoveNextAsync()
+                    go <- step
+
+        | TryFilterAsync filterer ->
+            while go do
+                let current = e.Current
+
+                match! filterer current with
+                | true ->
+                    foundItem <- Some current
+                    go <- false
+                | false ->
+                    let! step = e.MoveNextAsync()
+                    go <- step
+
+        return foundItem
+    }
+
+    /// Supports all four types of chosing: choose/filter/choose-async/filter-async
+    let filter chooser (taskSeq': taskSeq<_>) = taskSeq {
+        match chooser with
+        | TryPick picker ->
+            for item in taskSeq' do
+                match picker item with
+                | Some value -> yield value
+                | None -> ()
+
+        | TryPickAsync picker ->
+            for item in taskSeq' do
+                match! picker item with
+                | Some value -> yield value
+                | None -> ()
+
+        | TryFilter filterer ->
+            for item in taskSeq' do
+                if filterer item then
+                    yield item
+
+        | TryFilterAsync filterer ->
+            for item in taskSeq' do
+                match! filterer item with
+                | true -> yield item
+                | false -> ()
+    }
