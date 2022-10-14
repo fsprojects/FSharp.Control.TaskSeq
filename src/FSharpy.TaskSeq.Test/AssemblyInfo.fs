7724332901b44a2e4f8a2bfd2a0977f057bd2595
Abel Braaksma <abel.online@xs4all.nl>
2022-10-14 22:25:13 +0200

    Set blame timeout to 60s and re-enable parallelization
    Re-enable parallelization
    
    Blame-timeout to 60s

--- src/FSharpy.TaskSeq.Test/AssemblyInfo.fs
+++ src/FSharpy.TaskSeq.Test/AssemblyInfo.fs
@@ -2,7 +2,7 @@ namespace FSharpy.Tests
 
 open System.Runtime.CompilerServices
 
-[<assembly: Xunit.CollectionBehavior(DisableTestParallelization = true)>]
+[<assembly: Xunit.CollectionBehavior(DisableTestParallelization = false)>]
 [<assembly: Xunit.TestCaseOrderer("FSharpy.Tests.AlphabeticalOrderer", "FSharpy.TaskSeq.Test")>]
 
 do ()
