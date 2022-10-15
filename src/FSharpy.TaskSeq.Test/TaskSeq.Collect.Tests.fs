8e24bf9ef26d0554701533af02a9eb4a7a512a0b
Abel Braaksma <abel.online@xs4all.nl>
2022-10-16 01:40:30 +0200

    Add TaskSeq.collectAsync and TaskSeq.collectSeqAsync tests and improve existing ones

--- src/FSharpy.TaskSeq.Test/TaskSeq.Collect.Tests.fs
+++ src/FSharpy.TaskSeq.Test/TaskSeq.Collect.Tests.fs
@@ -6,6 +6,12 @@ open FsToolkit.ErrorHandling
 
 open FSharpy
 
+let validateSequence sequence =
+    sequence
+    |> Seq.map string
+    |> String.concat ""
+    |> should equal "ABBCCDDEEFFGGHHIIJJK"
+
 [<Fact>]
 let ``TaskSeq-collect operates in correct order`` () = task {
     let! sq =
@@ -16,10 +22,22 @@ let ``TaskSeq-collect operates in correct order`` () = task {
         })
         |> TaskSeq.toSeqCachedAsync
 
-    sq
-    |> Seq.map string
-    |> String.concat ""
-    |> should equal "ABBCCDDEEFFGGHHIIJJK"
+    validateSequence sq
+}
+
+[<Fact>]
+let ``TaskSeq-collectAsync operates in correct order`` () = task {
+    let! sq =
+        createDummyTaskSeq 10
+        |> TaskSeq.collectAsync (fun item -> task {
+            return taskSeq {
+                yield char (item + 64)
+                yield char (item + 65)
+            }
+        })
+        |> TaskSeq.toSeqCachedAsync
+
+    validateSequence sq
 }
 
 [<Fact>]
@@ -32,10 +50,42 @@ let ``TaskSeq-collectSeq operates in correct order`` () = task {
         })
         |> TaskSeq.toSeqCachedAsync
 
-    sq
-    |> Seq.map string
-    |> String.concat ""
-    |> should equal "ABBCCDDEEFFGGHHIIJJK"
+    validateSequence sq
+}
+
+[<Fact>]
+let ``TaskSeq-collectSeq with arrays operates in correct order`` () = task {
+    let! sq =
+        createDummyTaskSeq 10
+        |> TaskSeq.collectSeq (fun item -> [| char (item + 64); char (item + 65) |])
+        |> TaskSeq.toArrayAsync
+
+    validateSequence sq
+}
+
+[<Fact>]
+let ``TaskSeq-collectSeqAsync operates in correct order`` () = task {
+    let! sq =
+        createDummyTaskSeq 10
+        |> TaskSeq.collectSeqAsync (fun item -> task {
+            return seq {
+                yield char (item + 64)
+                yield char (item + 65)
+            }
+        })
+        |> TaskSeq.toSeqCachedAsync
+
+    validateSequence sq
+}
+
+[<Fact>]
+let ``TaskSeq-collectSeqAsync with arrays operates in correct order`` () = task {
+    let! sq =
+        createDummyTaskSeq 10
+        |> TaskSeq.collectSeqAsync (fun item -> task { return [| char (item + 64); char (item + 65) |] })
+        |> TaskSeq.toArrayAsync
+
+    validateSequence sq
 }
 
 [<Fact>]
@@ -49,10 +99,30 @@ let ``TaskSeq-collect with empty task sequences`` () = task {
 }
 
 [<Fact>]
+let ``TaskSeq-collectAsync with empty task sequences`` () = task {
+    let! sq =
+        createDummyTaskSeq 10
+        |> TaskSeq.collectAsync (fun _ -> task { return TaskSeq.empty<string> })
+        |> TaskSeq.toSeqCachedAsync
+
+    Seq.isEmpty sq |> should be True
+}
+
+[<Fact>]
 let ``TaskSeq-collectSeq with empty sequences`` () = task {
     let! sq =
         createDummyTaskSeq 10
         |> TaskSeq.collectSeq (fun _ -> Seq.empty<int>)
+        |> TaskSeq.toSeqCachedAsync
+
+    Seq.isEmpty sq |> should be True
+}
+
+[<Fact>]
+let ``TaskSeq-collectSeqAsync with empty sequences`` () = task {
+    let! sq =
+        createDummyTaskSeq 10
+        |> TaskSeq.collectSeqAsync (fun _ -> task { return Array.empty<int> })
         |> TaskSeq.toSeqCachedAsync
 
     Seq.isEmpty sq |> should be True
