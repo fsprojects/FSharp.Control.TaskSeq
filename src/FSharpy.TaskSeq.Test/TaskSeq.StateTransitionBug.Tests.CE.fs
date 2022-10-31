9307a1ba8d2ace80d9ed2573a57e70b870468293
Abel Braaksma <abel.online@xs4all.nl>
2022-11-01 00:23:16 +0100

    Rename and cleanup test

--- src/FSharpy.TaskSeq.Test/TaskSeq.StateTransitionBug.Tests.CE.fs
+++ src/FSharpy.TaskSeq.Test/TaskSeq.StateTransitionBug.Tests.CE.fs
@@ -48,9 +48,10 @@ let ``CE  empty taskSeq, GetAsyncEnumerator multiple times and then MoveNextAsyn
     do! Assert.moveNextAndCheck false enumerator
 }
 
-// TODO: shaky test? Appears that this occasionally raises a NullReferenceException,
+// FIXED!
+// Previously: shaky test. Appears that this occasionally raises a NullReferenceException,
 // esp when there's stress (i.e. run all at the same time).
-// See TRACE_FAIL/SUCCESS in Traces folder.
+// See https://github.com/abelbraaksma/TaskSeq/pull/54
 [<Theory; ClassData(typeof<TestEmptyVariants>)>]
 let ``CE empty taskSeq, GetAsyncEnumerator + MoveNextAsync multiple times`` variant = task {
     let tskSeq = Gen.getEmptyVariant variant
@@ -63,27 +64,23 @@ let ``CE empty taskSeq, GetAsyncEnumerator + MoveNextAsync multiple times`` vari
     do! Assert.moveNextAndCheck false enumerator2 // new hone should also work without raising
 }
 
+// FIXED!
+// This is the simpler version of the above test.
 [<Fact>]
-let ``BUG CE empty taskSeq, GetAsyncEnumerator + MoveNextAsync multiple times`` () = task {
-    //for i in 1..10 do
-    //    printfn "\nSTART TEST"
-
-    let tskSeq = taskSeq {
-        do! Task.Delay(50) |> Task.ofTask
-    //yield 10
-    }
+let ``BUG #54 CE with empty taskSeq and Delay, crash after GetAsyncEnumerator + MoveNextAsync 2x`` () = task {
+    // See: https://github.com/abelbraaksma/TaskSeq/pull/54
+    let tskSeq = taskSeq { do! Task.Delay(50) |> Task.ofTask }
 
     use enumerator1 = tskSeq.GetAsyncEnumerator()
-    printfn ">>> FIRST MOVENEXT"
     let! (hasNext: bool) = enumerator1.MoveNextAsync()
+    hasNext |> should be False
 
     use enumerator1 = tskSeq.GetAsyncEnumerator()
-    printfn ">>> SECOND MOVENEXT"
     let! (hasNext: bool) = enumerator1.MoveNextAsync()
-    printfn ">>> THIRD MOVENEXT"
-    let! (hasNext: bool) = enumerator1.MoveNextAsync()
-    printfn ">>> END"
-    ()
+    hasNext |> should be False
+
+    let! (hasNext: bool) = enumerator1.MoveNextAsync() // fail here
+    hasNext |> should be False
 }
 
 [<Theory; ClassData(typeof<TestEmptyVariants>)>]
