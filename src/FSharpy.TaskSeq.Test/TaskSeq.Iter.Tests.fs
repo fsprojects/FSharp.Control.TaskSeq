a72f7f355696f029b37e18e05deba6178bf7fdf7
Abel Braaksma <abel.online@xs4all.nl>
2022-10-31 04:18:14 +0100

    Use test-data generators for TaskSeq.iter/iteri tests

--- src/FSharpy.TaskSeq.Test/TaskSeq.Iter.Tests.fs
+++ src/FSharpy.TaskSeq.Test/TaskSeq.Iter.Tests.fs
@@ -5,70 +5,125 @@ open FsUnit.Xunit
 
 open FSharpy
 
-[<Fact>]
-let ``TaskSeq-iteri does nothing on empty sequences`` () = task {
-    let tq = Gen.sideEffectTaskSeq 10
-    let mutable sum = -1
-    do! TaskSeq.empty |> TaskSeq.iteri (fun i _ -> sum <- sum + i)
-    sum |> should equal -1
-}
+module EmptySeq =
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-iteri does nothing on empty sequences`` variant = task {
+        let tq = Gen.getEmptyVariant variant
+        let mutable sum = -1
+        do! tq |> TaskSeq.iteri (fun i _ -> sum <- sum + i)
+        sum |> should equal -1
+    }
 
-[<Fact>]
-let ``TaskSeq-iter does nothing on empty sequences`` () = task {
-    let tq = Gen.sideEffectTaskSeq 10
-    let mutable sum = -1
-    do! TaskSeq.empty |> TaskSeq.iter (fun i -> sum <- sum + i)
-    sum |> should equal -1
-}
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-iter does nothing on empty sequences`` variant = task {
+        let tq = Gen.getEmptyVariant variant
+        let mutable sum = -1
+        do! tq |> TaskSeq.iter (fun i -> sum <- sum + i)
+        sum |> should equal -1
+    }
 
-[<Fact>]
-let ``TaskSeq-iteri should go over all items`` () = task {
-    let tq = Gen.sideEffectTaskSeq 10
-    let mutable sum = 0
-    do! tq |> TaskSeq.iteri (fun i _ -> sum <- sum + i)
-    sum |> should equal 45 // index starts at 0
-}
+module Immutable =
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-iteri should go over all items`` variant = task {
+        let tq = Gen.getSeqImmutable variant
+        let mutable sum = 0
+        do! tq |> TaskSeq.iteri (fun i _ -> sum <- sum + i)
+        sum |> should equal 45 // index starts at 0
+    }
 
-[<Fact>]
-let ``TaskSeq-iter should go over all items`` () = task {
-    let tq = Gen.sideEffectTaskSeq 10
-    let mutable sum = 0
-    do! tq |> TaskSeq.iter (fun item -> sum <- sum + item)
-    sum |> should equal 55 // task-dummies started at 1
-}
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-iter should go over all items`` variant = task {
+        let tq = Gen.getSeqImmutable variant
+        let mutable sum = 0
+        do! tq |> TaskSeq.iter (fun item -> sum <- sum + item)
+        sum |> should equal 55 // task-dummies started at 1
+    }
 
 
-[<Fact>]
-let ``TaskSeq-iter multiple iterations over same sequence`` () = task {
-    let tq = Gen.sideEffectTaskSeq 10
-    let mutable sum = 0
-    do! tq |> TaskSeq.iter (fun item -> sum <- sum + item)
-    do! tq |> TaskSeq.iter (fun item -> sum <- sum + item)
-    do! tq |> TaskSeq.iter (fun item -> sum <- sum + item)
-    do! tq |> TaskSeq.iter (fun item -> sum <- sum + item)
-    sum |> should equal 820 // task-dummies started at 1
-}
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-iter multiple iterations over same sequence`` variant = task {
+        let tq = Gen.getSeqImmutable variant
+        let mutable sum = 0
+        do! tq |> TaskSeq.iter (fun item -> sum <- sum + item)
+        do! tq |> TaskSeq.iter (fun item -> sum <- sum + item)
+        do! tq |> TaskSeq.iter (fun item -> sum <- sum + item)
+        do! tq |> TaskSeq.iter (fun item -> sum <- sum + item)
+        sum |> should equal 220 // immutable tasks, so 'item' does not change, just 4 x 55
+    }
 
-[<Fact>]
-let ``TaskSeq-iteriAsync should go over all items`` () = task {
-    let tq = Gen.sideEffectTaskSeq 10
-    let mutable sum = 0
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-iteriAsync should go over all items`` variant = task {
+        let tq = Gen.getSeqImmutable variant
+        let mutable sum = 0
 
-    do!
-        tq
-        |> TaskSeq.iteriAsync (fun i _ -> task { sum <- sum + i })
+        do!
+            tq
+            |> TaskSeq.iteriAsync (fun i _ -> task { sum <- sum + i })
 
-    sum |> should equal 45 // index starts at 0
-}
+        sum |> should equal 45 // index starts at 0
+    }
 
-[<Fact>]
-let ``TaskSeq-iterAsync should go over all items`` () = task {
-    let tq = Gen.sideEffectTaskSeq 10
-    let mutable sum = 0
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-iterAsync should go over all items`` variant = task {
+        let tq = Gen.getSeqImmutable variant
+        let mutable sum = 0
 
-    do!
-        tq
-        |> TaskSeq.iterAsync (fun item -> task { sum <- sum + item })
+        do!
+            tq
+            |> TaskSeq.iterAsync (fun item -> task { sum <- sum + item })
 
-    sum |> should equal 55 // task-dummies started at 1
-}
+        sum |> should equal 55 // task-dummies started at 1
+    }
+
+module SideEffects =
+    [<Theory; ClassData(typeof<TestSideEffectTaskSeq>)>]
+    let ``TaskSeq-iteri should go over all items`` variant = task {
+        let tq = Gen.getSeqWithSideEffect variant
+        let mutable sum = 0
+        do! tq |> TaskSeq.iteri (fun i _ -> sum <- sum + i)
+        sum |> should equal 45 // index starts at 0
+    }
+
+    [<Theory; ClassData(typeof<TestSideEffectTaskSeq>)>]
+    let ``TaskSeq-iter should go over all items`` variant = task {
+        let tq = Gen.getSeqWithSideEffect variant
+        let mutable sum = 0
+        do! tq |> TaskSeq.iter (fun item -> sum <- sum + item)
+        sum |> should equal 55 // task-dummies started at 1
+    }
+
+
+    [<Theory; ClassData(typeof<TestSideEffectTaskSeq>)>]
+    let ``TaskSeq-iter multiple iterations over same sequence`` variant = task {
+        let tq = Gen.getSeqWithSideEffect variant
+        let mutable sum = 0
+        do! tq |> TaskSeq.iter (fun item -> printfn "A:%i" item; sum <- sum + item)
+        do! tq |> TaskSeq.iter (fun item -> printfn "B:%i" item; sum <- sum + item)
+        do! tq |> TaskSeq.iter (fun item -> printfn "C:%i" item; sum <- sum + item)
+        do! tq |> TaskSeq.iter (fun item -> printfn "D:%i" item; sum <- sum + item)
+        sum |> should equal 820 // side-effected tasks, so 'item' DOES CHANGE, each next iteration starts 10 higher
+    }
+
+    [<Theory; ClassData(typeof<TestSideEffectTaskSeq>)>]
+    let ``TaskSeq-iteriAsync should go over all items`` variant = task {
+        let tq = Gen.getSeqWithSideEffect variant
+        let mutable sum = 0
+
+        do!
+            tq
+            |> TaskSeq.iteriAsync (fun i _ -> task { sum <- sum + i })
+
+        sum |> should equal 45 // index starts at 0
+    }
+
+    [<Theory; ClassData(typeof<TestSideEffectTaskSeq>)>]
+    let ``TaskSeq-iterAsync should go over all items`` variant = task {
+        let tq = Gen.getSeqWithSideEffect variant
+        let mutable sum = 0
+
+        do!
+            tq
+            |> TaskSeq.iterAsync (fun item -> task { sum <- sum + item })
+
+        sum |> should equal 55 // task-dummies started at 1
+    }
