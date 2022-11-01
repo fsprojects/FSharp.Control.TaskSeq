82c658b67b0c7486c14b6ee18a20b5065afeece5
Abel Braaksma <abel.online@xs4all.nl>
2022-11-01 03:29:53 +0100

    Added tests for TaskSeq.filter, filterAsync, find, tryFind, findAsync, tryFindAsync

--- src/FSharpy.TaskSeq.Test/TaskSeq.Find.Tests.fs
+++ src/FSharpy.TaskSeq.Test/TaskSeq.Find.Tests.fs
@@ -7,241 +7,174 @@ open FsToolkit.ErrorHandling
 open FSharpy
 open System.Collections.Generic
 
-//
-// TaskSeq.find
-// TaskSeq.findAsync
-// the tryXXX versions are at the bottom half
-//
+module EmptySeq =
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-find raises KeyNotFoundException`` variant =
+        fun () ->
+            Gen.getEmptyVariant variant
+            |> TaskSeq.find ((=) 12)
+            |> Task.ignore
+        |> should throwAsyncExact typeof<KeyNotFoundException>
 
-[<Fact>]
-let ``TaskSeq-find on an empty sequence raises KeyNotFoundException`` () = task {
-    fun () -> TaskSeq.empty |> TaskSeq.find ((=) 12) |> Task.ignore
-    |> should throwAsyncExact typeof<KeyNotFoundException>
-}
-
-[<Fact>]
-let ``TaskSeq-find on an empty sequence raises KeyNotFoundException - variant`` () = task {
-    fun () -> taskSeq { do () } |> TaskSeq.find ((=) 12) |> Task.ignore
-    |> should throwAsyncExact typeof<KeyNotFoundException>
-}
-
-[<Fact>]
-let ``TaskSeq-findAsync on an empty sequence raises KeyNotFoundException`` () = task {
-    fun () ->
-        TaskSeq.empty
-        |> TaskSeq.findAsync (fun x -> task { return x = 12 })
-        |> Task.ignore
-    |> should throwAsyncExact typeof<KeyNotFoundException>
-}
-
-[<Fact>]
-let ``TaskSeq-find sad path raises KeyNotFoundException`` () = task {
-    fun () ->
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.find ((=) 0) // dummy tasks sequence starts at 1
-        |> Task.ignore
-
-    |> should throwAsyncExact typeof<KeyNotFoundException>
-}
-
-[<Fact>]
-let ``TaskSeq-findAsync sad path raises KeyNotFoundException`` () = task {
-    fun () ->
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.findAsync (fun x -> task { return x = 0 }) // dummy tasks sequence starts at 1
-        |> Task.ignore
-
-    |> should throwAsyncExact typeof<KeyNotFoundException>
-}
-
-[<Fact>]
-let ``TaskSeq-find sad path raises KeyNotFoundException variant`` () = task {
-    fun () ->
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.find ((=) 51) // dummy tasks sequence ends at 50
-        |> Task.ignore
-
-    |> should throwAsyncExact typeof<KeyNotFoundException>
-}
-
-[<Fact>]
-let ``TaskSeq-findAsync sad path raises KeyNotFoundException variant`` () = task {
-    fun () ->
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.findAsync (fun x -> task { return x = 51 }) // dummy tasks sequence ends at 50
-        |> Task.ignore
-
-    |> should throwAsyncExact typeof<KeyNotFoundException>
-}
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-findAsync raises KeyNotFoundException`` variant =
+        fun () ->
+            Gen.getEmptyVariant variant
+            |> TaskSeq.findAsync (fun x -> task { return x = 12 })
+            |> Task.ignore
+        |> should throwAsyncExact typeof<KeyNotFoundException>
 
 
-[<Fact>]
-let ``TaskSeq-find happy path middle of seq`` () = task {
-    let! twentyFive =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.find (fun x -> x < 26 && x > 24)
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-tryFind returns None`` variant =
+        Gen.getEmptyVariant variant
+        |> TaskSeq.tryFind ((=) 12)
+        |> Task.map (should be None')
 
-    twentyFive |> should equal 25
-}
-
-[<Fact>]
-let ``TaskSeq-findAsync happy path middle of seq`` () = task {
-    let! twentyFive =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.findAsync (fun x -> task { return x < 26 && x > 24 })
-
-    twentyFive |> should equal 25
-}
-
-[<Fact>]
-let ``TaskSeq-find happy path first item of seq`` () = task {
-    let! first =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.find ((=) 1) // dummy tasks seq starts at 1
-
-    first |> should equal 1
-}
-
-[<Fact>]
-let ``TaskSeq-findAsync happy path first item of seq`` () = task {
-    let! first =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.findAsync (fun x -> task { return x = 1 }) // dummy tasks seq starts at 1
-
-    first |> should equal 1
-}
-
-[<Fact>]
-let ``TaskSeq-find happy path last item of seq`` () = task {
-    let! last =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.find ((=) 50) // dummy tasks seq ends at 50
-
-    last |> should equal 50
-}
-
-[<Fact>]
-let ``TaskSeq-findAsync happy path last item of seq`` () = task {
-    let! last =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.findAsync (fun x -> task { return x = 50 }) // dummy tasks seq ends at 50
-
-    last |> should equal 50
-}
-
-//
-// TaskSeq.tryFind
-// TaskSeq.tryFindAsync
-//
-
-[<Fact>]
-let ``TaskSeq-tryFind on an empty sequence returns None`` () = task {
-    let! nothing = TaskSeq.empty |> TaskSeq.tryFind ((=) 12)
-    nothing |> should be None'
-}
-
-[<Fact>]
-let ``TaskSeq-tryFindAsync on an empty sequence returns None`` () = task {
-    let! nothing =
-        TaskSeq.empty
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-tryFindAsync returns None`` variant =
+        Gen.getEmptyVariant variant
         |> TaskSeq.tryFindAsync (fun x -> task { return x = 12 })
+        |> Task.map (should be None')
 
-    nothing |> should be None'
-}
+module Immutable =
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-find sad path raises KeyNotFoundException`` variant =
+        fun () ->
+            Gen.getSeqImmutable variant
+            |> TaskSeq.find ((=) 0) // dummy tasks sequence starts at 1
+            |> Task.ignore
 
-[<Fact>]
-let ``TaskSeq-tryFind sad path returns None`` () = task {
-    let! nothing =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.tryFind ((=) 0) // dummy tasks sequence starts at 1
+        |> should throwAsyncExact typeof<KeyNotFoundException>
 
-    nothing |> should be None'
-}
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-findAsync sad path raises KeyNotFoundException`` variant =
+        fun () ->
+            Gen.getSeqImmutable variant
+            |> TaskSeq.findAsync (fun x -> task { return x = 0 }) // dummy tasks sequence starts at 1
+            |> Task.ignore
 
-[<Fact>]
-let ``TaskSeq-tryFindAsync sad path return None`` () = task {
-    let! nothing =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.tryFindAsync (fun x -> task { return x = 0 }) // dummy tasks sequence starts at 1
+        |> should throwAsyncExact typeof<KeyNotFoundException>
 
-    nothing |> should be None'
-}
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-find sad path raises KeyNotFoundException variant`` variant =
+        fun () ->
+            Gen.getSeqImmutable variant
+            |> TaskSeq.find ((=) 11)
+            |> Task.ignore
 
-[<Fact>]
-let ``TaskSeq-tryFind sad path returns None variant`` () = task {
-    let! nothing =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.tryFind ((<=) 51) // dummy tasks sequence ends at 50 (inverted sign in lambda!)
+        |> should throwAsyncExact typeof<KeyNotFoundException>
 
-    nothing |> should be None'
-}
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-findAsync sad path raises KeyNotFoundException variant`` variant =
+        fun () ->
+            Gen.getSeqImmutable variant
+            |> TaskSeq.findAsync (fun x -> task { return x = 11 })
+            |> Task.ignore
 
-[<Fact>]
-let ``TaskSeq-tryFindAsync sad path return None - variant`` () = task {
-    let! nothing =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.tryFindAsync (fun x -> task { return x >= 51 }) // dummy tasks sequence ends at 50
-
-    nothing |> should be None'
-}
+        |> should throwAsyncExact typeof<KeyNotFoundException>
 
 
-[<Fact>]
-let ``TaskSeq-tryFind happy path middle of seq`` () = task {
-    let! twentyFive =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.tryFind (fun x -> x < 26 && x > 24)
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-find happy path middle of seq`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.find (fun x -> x < 6 && x > 4)
+        |> Task.map (should equal 5)
 
-    twentyFive |> should be Some'
-    twentyFive |> should equal (Some 25)
-}
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-findAsync happy path middle of seq`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.findAsync (fun x -> task { return x < 6 && x > 4 })
+        |> Task.map (should equal 5)
 
-[<Fact>]
-let ``TaskSeq-tryFindAsync happy path middle of seq`` () = task {
-    let! twentyFive =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.tryFindAsync (fun x -> task { return x < 26 && x > 24 })
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-find happy path first item of seq`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.find ((=) 1)
+        |> Task.map (should equal 1)
 
-    twentyFive |> should be Some'
-    twentyFive |> should equal (Some 25)
-}
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-findAsync happy path first item of seq`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.findAsync (fun x -> task { return x = 1 })
+        |> Task.map (should equal 1)
 
-[<Fact>]
-let ``TaskSeq-tryFind happy path first item of seq`` () = task {
-    let! first =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.tryFind ((=) 1) // dummy tasks seq starts at 1
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-find happy path last item of seq`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.find ((=) 10)
+        |> Task.map (should equal 10)
 
-    first |> should be Some'
-    first |> should equal (Some 1)
-}
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-findAsync happy path last item of seq`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.findAsync (fun x -> task { return x = 10 }) // dummy tasks seq ends at 50
+        |> Task.map (should equal 10)
 
-[<Fact>]
-let ``TaskSeq-tryFindAsync happy path first item of seq`` () = task {
-    let! first =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.tryFindAsync (fun x -> task { return x = 1 }) // dummy tasks seq starts at 1
 
-    first |> should be Some'
-    first |> should equal (Some 1)
-}
+    //
+    //
+    // tryXXX stuff
+    //      |
+    //      |
+    //      V
 
-[<Fact>]
-let ``TaskSeq-tryFind happy path last item of seq`` () = task {
-    let! last =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.tryFind ((=) 50) // dummy tasks seq ends at 50
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-tryFind sad path returns None`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.tryFind ((=) 0)
+        |> Task.map (should be None')
 
-    last |> should be Some'
-    last |> should equal (Some 50)
-}
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-tryFindAsync sad path return None`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.tryFindAsync (fun x -> task { return x = 0 })
+        |> Task.map (should be None')
 
-[<Fact>]
-let ``TaskSeq-tryFindAsync happy path last item of seq`` () = task {
-    let! last =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.tryFindAsync (fun x -> task { return x = 50 }) // dummy tasks seq ends at 50
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-tryFind sad path returns None variant`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.tryFind ((<=) 11)
+        |> Task.map (should be None')
 
-    last |> should be Some'
-    last |> should equal (Some 50)
-}
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-tryFindAsync sad path return None - variant`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.tryFindAsync (fun x -> task { return x >= 11 })
+        |> Task.map (should be None')
+
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-tryFind happy path middle of seq`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.tryFind (fun x -> x < 6 && x > 4)
+        |> Task.map (should equal (Some 5))
+
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-tryFindAsync happy path middle of seq`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.tryFindAsync (fun x -> task { return x < 6 && x > 4 })
+        |> Task.map (should equal (Some 5))
+
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-tryFind happy path first item of seq`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.tryFind ((=) 1)
+        |> Task.map (should equal (Some 1))
+
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-tryFindAsync happy path first item of seq`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.tryFindAsync (fun x -> task { return x = 1 })
+        |> Task.map (should equal (Some 1))
+
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-tryFind happy path last item of seq`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.tryFind ((=) 10)
+        |> Task.map (should equal (Some 10))
+
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-tryFindAsync happy path last item of seq`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.tryFindAsync (fun x -> task { return x = 10 })
+        |> Task.map (should equal (Some 10))
