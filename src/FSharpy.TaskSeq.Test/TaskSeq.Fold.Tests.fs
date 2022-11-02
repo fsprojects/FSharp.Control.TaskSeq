3375e03cd763ffa658429b3d9fcbf97620f59266
Abel Braaksma <abel.online@xs4all.nl>
2022-11-02 23:11:27 +0100

    Add / improve TaskSeq.fold/foldAsync tests

--- src/FSharpy.TaskSeq.Test/TaskSeq.Fold.Tests.fs
+++ src/FSharpy.TaskSeq.Test/TaskSeq.Fold.Tests.fs
@@ -7,43 +7,77 @@ open FsToolkit.ErrorHandling
 
 open FSharpy
 
+module EmptySeq =
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-fold takes state when empty`` variant = task {
+        let! empty =
+            Gen.getEmptyVariant variant
+            |> TaskSeq.fold (fun _ item -> char (item + 64)) '_'
 
-[<Fact>]
-let ``TaskSeq-fold folds with every item`` () = task {
-    let! alphabet =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 26
-        |> TaskSeq.fold (fun (state: StringBuilder) item -> state.Append(char item + '@')) (StringBuilder())
+        empty |> should equal '_'
+    }
 
-    alphabet.ToString()
-    |> should equal "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-}
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-foldAsync takes state when empty`` variant = task {
+        let! alphabet =
+            Gen.getEmptyVariant variant
+            |> TaskSeq.foldAsync (fun _ item -> task { return char (item + 64) }) '_'
 
-[<Fact>]
-let ``TaskSeq-foldAsync folds with every item`` () = task {
-    let! alphabet =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 26
-        |> TaskSeq.foldAsync
-            (fun (state: StringBuilder) item -> task { return state.Append(char item + '@') })
-            (StringBuilder())
+        alphabet |> should equal '_'
+    }
 
-    alphabet.ToString()
-    |> should equal "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-}
+module Immutable =
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-fold folds with every item`` variant = task {
+        let! letters =
+            (StringBuilder(), Gen.getSeqImmutable variant)
+            ||> TaskSeq.fold (fun state item -> state.Append(char item + '@'))
 
-[<Fact>]
-let ``TaskSeq-fold takes state on empty IAsyncEnumberable`` () = task {
-    let! empty =
-        TaskSeq.empty
-        |> TaskSeq.fold (fun _ item -> char (item + 64)) '_'
+        letters.ToString()
+        |> should equal "ABCDEFGHIJ"
+    }
 
-    empty |> should equal '_'
-}
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-foldAsync folds with every item`` variant = task {
+        let! letters =
+            (StringBuilder(), Gen.getSeqImmutable variant)
+            ||> TaskSeq.foldAsync (fun state item -> task { return state.Append(char item + '@') })
+                
 
-[<Fact>]
-let ``TaskSeq-foldAsync takes state on empty IAsyncEnumerable`` () = task {
-    let! alphabet =
-        TaskSeq.empty
-        |> TaskSeq.foldAsync (fun _ item -> task { return char (item + 64) }) '_'
+        letters.ToString()
+        |> should equal "ABCDEFGHIJ"
+    }
 
-    alphabet |> should equal '_'
-}
+module SideEffects =
+    [<Theory; ClassData(typeof<TestSideEffectTaskSeq>)>]
+    let ``TaskSeq-fold folds with every item, next fold has different state`` variant = task {
+        let ts = Gen.getSeqWithSideEffect variant
+        let! letters =
+            (StringBuilder(), ts)
+            ||> TaskSeq.fold (fun state item -> state.Append(char item + '@'))
+
+        string letters |> should equal "ABCDEFGHIJ"
+
+        let! moreLetters =
+            (letters, ts)
+            ||> TaskSeq.fold (fun state item -> state.Append(char item + '@'))
+
+        string moreLetters |> should equal "ABCDEFGHIJKLMNOPQRST"
+    }
+
+    [<Theory; ClassData(typeof<TestSideEffectTaskSeq>)>]
+    let ``TaskSeq-foldAsync folds with every item, next fold has different state`` variant = task {
+        let ts = Gen.getSeqWithSideEffect variant
+        let! letters =
+            (StringBuilder(), ts)
+            ||> TaskSeq.foldAsync (fun state item -> task { return state.Append(char item + '@') })
+
+        string letters |> should equal "ABCDEFGHIJ"
+
+        let! moreLetters =
+            (letters, ts)
+            ||> TaskSeq.foldAsync (fun state item -> task { return state.Append(char item + '@') })
+
+        string moreLetters |> should equal "ABCDEFGHIJKLMNOPQRST"
+    }
+
