db64dbca717980602cae3b3a4bd107ee8d6a9f8a
Abel Braaksma <abel.online@xs4all.nl>
2022-11-03 19:25:33 +0100

    Implement TaskSeq.indexed + tests & docs

---
+++ src/FSharpy.TaskSeq.Test/TaskSeq.Indexed.Tests.fs
@@ -0,0 +1,34 @@
+module FSharpy.Tests.Indexed
+
+open Xunit
+open FsUnit.Xunit
+open FsToolkit.ErrorHandling
+
+open FSharpy
+
+//
+// TaskSeq.indexed
+//
+
+module EmptySeq =
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-indexed on empty`` variant =
+        Gen.getEmptyVariant variant
+        |> TaskSeq.indexed
+        |> verifyEmpty
+
+module Immutable =
+    [<Fact>]
+    let ``TaskSeq-indexed starts at zero`` () =
+        taskSeq { yield 99 }
+        |> TaskSeq.indexed
+        |> TaskSeq.head
+        |> Task.map (should equal (0, 99))
+
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-indexed`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.indexed
+        |> TaskSeq.toArrayAsync
+        |> Task.map (Array.forall (fun (x, y) -> x + 1 = y))
+        |> Task.map (should be True)
