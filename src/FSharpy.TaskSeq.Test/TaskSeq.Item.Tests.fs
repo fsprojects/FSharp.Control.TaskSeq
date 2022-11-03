75ccf78e8ffa1fbb42d94c2d979d5c86239e246a
Abel Braaksma <abel.online@xs4all.nl>
2022-11-03 01:15:30 +0100

    Improve and expand tests for TaskSeq.item/tryItem, found bug, see #65

--- src/FSharpy.TaskSeq.Test/TaskSeq.Item.Tests.fs
+++ src/FSharpy.TaskSeq.Test/TaskSeq.Item.Tests.fs
@@ -8,240 +8,300 @@ open FsToolkit.ErrorHandling
 open FSharpy
 
 
-[<Fact>]
-let ``TaskSeq-item throws on empty sequences`` () = task {
-    fun () -> TaskSeq.empty<string> |> TaskSeq.item 0 |> Task.ignore
-    |> should throwAsyncExact typeof<ArgumentException>
-}
+module EmptySeq =
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-item throws on empty sequences`` variant = task {
+        fun () -> Gen.getEmptyVariant variant |> TaskSeq.item 0 |> Task.ignore
+        |> should throwAsyncExact typeof<ArgumentException>
+    }
 
-[<Fact>]
-let ``TaskSeq-item throws on empty sequence - variant`` () = task {
-    fun () -> taskSeq { do () } |> TaskSeq.item 50000 |> Task.ignore
-    |> should throwAsyncExact typeof<ArgumentException>
-}
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-item throws on empty sequence - high index`` variant = task {
+        fun () ->
+            Gen.getEmptyVariant variant
+            |> TaskSeq.item 50000
+            |> Task.ignore
+        |> should throwAsyncExact typeof<ArgumentException>
+    }
 
-[<Fact>]
-let ``TaskSeq-item throws when not found`` () = task {
-    fun () ->
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.item 51
-        |> Task.ignore
-    |> should throwAsyncExact typeof<ArgumentException>
-}
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-tryItem returns None on empty sequences`` variant = task {
+        let! nothing = Gen.getEmptyVariant variant |> TaskSeq.tryItem 0
+        nothing |> should be None'
+    }
 
-[<Fact>]
-let ``TaskSeq-item throws when not found - variant`` () = task {
-    fun () ->
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.item Int32.MaxValue
-        |> Task.ignore
-    |> should throwAsyncExact typeof<ArgumentException>
-}
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-tryItem returns None on empty sequence - high index`` variant = task {
+        let! nothing = Gen.getEmptyVariant variant |> TaskSeq.tryItem 50000
+        nothing |> should be None'
+    }
 
-[<Fact>]
-let ``TaskSeq-item throws when accessing 2nd item in singleton sequence`` () = task {
-    fun () -> taskSeq { yield 10 } |> TaskSeq.item 1 |> Task.ignore // zero-based!
-    |> should throwAsyncExact typeof<ArgumentException>
-}
+module Singleton =
+    [<Fact>]
+    let ``TaskSeq-item gets the first item in a singleton sequence`` () = task {
+        let! head = taskSeq { yield 10 } |> TaskSeq.item 0 // zero-based!
+        head |> should equal 10
+    }
 
-[<Fact>]
-let ``TaskSeq-item always throws with negative values`` () = task {
-    let make50 () = Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
+    [<Fact>]
+    let ``TaskSeq-tryItem gets the first item in a singleton sequence`` () = task {
+        let! head = taskSeq { yield 10 } |> TaskSeq.tryItem 0 // zero-based!
+        head |> should be Some'
+        head |> should equal (Some 10)
+    }
 
-    fun () -> make50 () |> TaskSeq.item -1 |> Task.ignore
-    |> should throwAsyncExact typeof<ArgumentException>
+    [<Fact>]
+    let ``TaskSeq-item throws when accessing 2nd item in singleton sequence`` () = task {
+        fun () -> taskSeq { yield 10 } |> TaskSeq.item 1 |> Task.ignore // zero-based!
+        |> should throwAsyncExact typeof<ArgumentException>
+    }
 
-    fun () -> make50 () |> TaskSeq.item -10000 |> Task.ignore
-    |> should throwAsyncExact typeof<ArgumentException>
+    [<Fact>]
+    let ``TaskSeq-tryItem returns None when accessing 2nd item in singleton sequence`` () = task {
+        let! nothing = taskSeq { yield 10 } |> TaskSeq.tryItem 1 // zero-based!
+        nothing |> should be None'
+    }
 
-    fun () -> make50 () |> TaskSeq.item Int32.MinValue |> Task.ignore
-    |> should throwAsyncExact typeof<ArgumentException>
+module Immutable =
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-item throws when not found`` variant = task {
+        fun () ->
+            Gen.getSeqImmutable variant
+            |> TaskSeq.item 10
+            |> Task.ignore
+        |> should throwAsyncExact typeof<ArgumentException>
+    }
 
-    fun () -> TaskSeq.empty<string> |> TaskSeq.item -1 |> Task.ignore
-    |> should throwAsyncExact typeof<ArgumentException>
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-tryItem returns None when not found`` variant = task {
+        let! nothing = Gen.getSeqImmutable variant |> TaskSeq.tryItem 10 // zero-based index
 
-    fun () -> TaskSeq.empty<string> |> TaskSeq.item -10000 |> Task.ignore
-    |> should throwAsyncExact typeof<ArgumentException>
+        nothing |> should be None'
+    }
 
-    fun () ->
-        TaskSeq.empty<string>
-        |> TaskSeq.item Int32.MinValue
-        |> Task.ignore
-    |> should throwAsyncExact typeof<ArgumentException>
-}
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-item can get the first and last item in a longer sequence`` variant = task {
+        let! head = Gen.getSeqImmutable variant |> TaskSeq.item 0
+        let! tail = Gen.getSeqImmutable variant |> TaskSeq.item 9
+        head |> should equal 1
+        tail |> should equal 10
+    }
 
-[<Fact>]
-let ``TaskSeq-tryItem returns None on empty sequences`` () = task {
-    let! nothing = TaskSeq.empty<string> |> TaskSeq.tryItem 0
-    nothing |> should be None'
-}
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-tryItem can get the first and last item in a longer sequence`` variant = task {
+        let! head = Gen.getSeqImmutable variant |> TaskSeq.tryItem 0 // zero-based!
+        let! tail = Gen.getSeqImmutable variant |> TaskSeq.tryItem 9
 
-[<Fact>]
-let ``TaskSeq-tryItem returns None on empty sequence - variant`` () = task {
-    let! nothing = taskSeq { do () } |> TaskSeq.tryItem 50000
-    nothing |> should be None'
-}
+        head |> should equal (Some 1)
+        tail |> should equal (Some 10)
+    }
 
-[<Fact>]
-let ``TaskSeq-tryItem returns None when not found`` () = task {
-    let! nothing =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.tryItem 50 // zero-based index, so a sequence of 50 items has its last item at index 49
+module SideEffect =
+    [<Theory; ClassData(typeof<TestSideEffectTaskSeq>)>]
+    let ``TaskSeq-item prove it searches the whole sequence`` variant = task {
+        let ts = Gen.getSeqWithSideEffect variant
 
-    nothing |> should be None'
-}
+        fun () -> ts |> TaskSeq.item 10 |> Task.ignore
+        |> should throwAsyncExact typeof<ArgumentException>
 
-[<Fact>]
-let ``TaskSeq-tryItem returns None when not found - variant`` () = task {
-    let! nothing =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.tryItem Int32.MaxValue
+        let! head = TaskSeq.head ts
+        head |> should equal 11 // all side effects have executed
+    }
 
-    nothing |> should be None'
-}
+    [<Theory; ClassData(typeof<TestSideEffectTaskSeq>)>]
+    let ``TaskSeq-tryItem prove it searches the whole sequence`` variant = task {
+        let ts = Gen.getSeqWithSideEffect variant
+        let! item = ts |> TaskSeq.tryItem 10
 
-[<Fact>]
-let ``TaskSeq-tryItem returns None when accessing 2nd item in singleton sequence`` () = task {
-    let! nothing = taskSeq { yield 10 } |> TaskSeq.tryItem 1 // zero-based!
-    nothing |> should be None'
-}
+        item |> should be None'
+        let! head = TaskSeq.head ts
+        head |> should equal 11 // all side effects have executed
+    }
 
-[<Fact>]
-let ``TaskSeq-tryItem returns None throws with negative values`` () = task {
-    let make50 () = Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
+    [<Fact>]
+    let ``TaskSeq-item prove we don't iterate further than the found item`` () = task {
+        let mutable i = 0
 
-    let! nothing = make50 () |> TaskSeq.tryItem -1
-    nothing |> should be None'
-
-    let! nothing = make50 () |> TaskSeq.tryItem -10000
-    nothing |> should be None'
-
-    let! nothing = make50 () |> TaskSeq.tryItem Int32.MinValue
-    nothing |> should be None'
-
-    let! nothing = TaskSeq.empty<string> |> TaskSeq.tryItem -1
-    nothing |> should be None'
-
-    let! nothing = TaskSeq.empty<string> |> TaskSeq.tryItem -10000
-    nothing |> should be None'
-
-    let! nothing = TaskSeq.empty<string> |> TaskSeq.tryItem Int32.MinValue
-    nothing |> should be None'
-}
-
-[<Fact>]
-let ``TaskSeq-item can get the first item in a longer sequence`` () = task {
-    let! head =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.item 0
-
-    head |> should equal 1
-}
-
-[<Fact>]
-let ``TaskSeq-item can get the last item in a longer sequence`` () = task {
-    let! head =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.item 49
-
-    head |> should equal 50
-}
-
-[<Fact>]
-let ``TaskSeq-item can get the first item in a singleton sequence`` () = task {
-    let! head = taskSeq { yield 10 } |> TaskSeq.item 0 // zero-based index!
-    head |> should equal 10
-}
-
-[<Fact>]
-let ``TaskSeq-tryItem can get the first item in a longer sequence`` () = task {
-    let! head =
-        Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
-        |> TaskSeq.tryItem 0 // zero-based!
-
-    head |> should be Some'
-    head |> should equal (Some 1)
-}
-
-[<Fact>]
-let ``TaskSeq-tryItem in a very long sequence (5_000 items - slow variant)`` () = task {
-    let! head =
-        Gen.sideEffectTaskSeq_Sequential 5_001
-        |> TaskSeq.tryItem 5_000 // zero-based!
-
-    head |> should be Some'
-    head |> should equal (Some 5_001)
-}
-
-[<Fact>]
-let ``TaskSeq-tryItem in a very long sequence (50_000 items - slow variant)`` () = task {
-    let! head =
-        Gen.sideEffectTaskSeq_Sequential 50_001
-        |> TaskSeq.tryItem 50_000 // zero-based!
-
-    head |> should be Some'
-    head |> should equal (Some 50_001)
-}
-
-[<Fact>]
-let ``TaskSeq-tryItem in a very long sequence (50_000 items - fast variant)`` () = task {
-    let! head =
-        // using taskSeq instead of the delayed-task approach above, which creates an extra closure for each
-        // task, we can really see the speed of the 'taskSeq' CE!! This is
-        taskSeq {
-            for i in [ 0..50_000 ] do
-                yield i
+        let ts = taskSeq {
+            i <- i + 1
+            yield 1
+            i <- i + 1
+            yield 2
+            i <- i + 1 // we never get here
         }
-        |> TaskSeq.tryItem 50_000 // zero-based!
 
-    head |> should be Some'
-    head |> should equal (Some 50_000)
-}
+        // zero-based index
+        do! ts |> TaskSeq.item 1 |> Task.map (should equal 2)
+        i |> should equal 2 // last side effect is not executed
+    }
 
-[<Fact>]
-let ``TaskSeq-tryItem in a very long sequence (50_000 items - using sync Seq)`` () = task {
-    // this test is just for smoke-test perf comparison with TaskSeq above
-    let head =
-        seq {
-            for i in [ 0..50_000 ] do
-                yield i
+    [<Fact>]
+    let ``TaskSeq-tryItem prove we don't iterate further than the found item`` () = task {
+        let mutable i = 0
+
+        let ts = taskSeq {
+            i <- i + 1
+            yield 1
+            i <- i + 1
+            yield 2
+            i <- i + 1 // we never get here
         }
-        |> Seq.tryItem 50_000 // zero-based!
 
-    head |> should be Some'
-    head |> should equal (Some 50_000)
-}
+        // zero-based index
+        do! ts |> TaskSeq.tryItem 1 |> Task.map (should equal (Some 2))
+        i |> should equal 2 // last side effect is not executed
+    }
 
-[<Fact>]
-let ``TaskSeq-tryItem in a very long sequence (500_000 items - fast variant)`` () = task {
-    let! head =
-        taskSeq {
-            for i in [ 0..500_000 ] do
-                yield i
+    [<Fact>]
+    let ``TaskSeq-item prove we iterate beyond the end when not found`` () = task {
+        let mutable i = 0
+
+        let ts = taskSeq {
+            i <- i + 1
+            yield 1
+            i <- i + 1
+            yield 2
+            i <- i + 1 // we never get here
         }
-        |> TaskSeq.tryItem 500_000 // zero-based!
 
-    head |> should be Some'
-    head |> should equal (Some 500_000)
-}
+        // zero-based
+        fun () -> ts |> TaskSeq.item 2 |> Task.ignore
+        |> should throwAsyncExact typeof<ArgumentException>
 
-[<Fact>]
-let ``TaskSeq-tryItem in a very long sequence (500_000 items - using sync Seq)`` () = task {
-    // this test is just for smoke-test perf comparison with TaskSeq above
-    let head =
-        seq {
-            for i in [ 0..500_000 ] do
-                yield i
+        i |> should equal 3 // last side effect MUST be executed
+    }
+
+    [<Fact>]
+    let ``TaskSeq-tryItem prove we iterate beyond the end when not found`` () = task {
+        let mutable i = 0
+
+        let ts = taskSeq {
+            i <- i + 1
+            yield 1
+            i <- i + 1
+            yield 2
+            i <- i + 1 // we never get here
         }
-        |> Seq.tryItem 500_000 // zero-based!
 
-    head |> should be Some'
-    head |> should equal (Some 500_000)
-}
+        do! ts |> TaskSeq.tryItem 2 |> Task.map (should be None')
+        i |> should equal 3 // last side effect MUST be executed
+    }
 
-[<Fact>]
-let ``TaskSeq-tryItem gets the first item in a singleton sequence`` () = task {
-    let! head = taskSeq { yield 10 } |> TaskSeq.tryItem 0 // zero-based!
-    head |> should be Some'
-    head |> should equal (Some 10)
-}
+
+module Performance =
+
+    [<Theory; InlineData 10_000; InlineData 100_000; InlineData 1_000_000>]
+    let ``TaskSeq-tryItem in a very long sequence -- yield variant`` total = task {
+        let! head =
+            taskSeq {
+                for i in [ 0..total ] do
+                    yield i
+            }
+            |> TaskSeq.tryItem total // zero-based!
+
+        head |> should equal (Some total)
+    }
+
+    [<Theory; InlineData 10_000; InlineData 100_000; InlineData 1_000_000>]
+    let ``[compare] Seq-tryItem in a very long sequence -- yield using F# Seq`` total = task {
+        // this test is just for smoke-test perf comparison with TaskSeq above
+        let head =
+            seq {
+                for i in [ 0..total ] do
+                    yield i
+            }
+            |> Seq.tryItem total // zero-based!
+
+        head |> should equal (Some total)
+    }
+
+    [<Theory; InlineData 10_000; InlineData 100_000; InlineData 1_000_000>]
+    let ``TaskSeq-tryItem in a very long sequence -- array variant`` total = task {
+        let! head = taskSeq { yield! [| 0..total |] } |> TaskSeq.tryItem total // zero-based!
+
+        head |> should equal (Some total)
+    }
+
+    [<Theory; InlineData 10_000; InlineData 100_000; InlineData 1_000_000>]
+    let ``[compare] Seq-tryItem in a very long sequence -- array using F# Seq`` total = task {
+        // this test is just for smoke-test perf comparison with TaskSeq above
+        let head = seq { yield! [| 0..total |] } |> Seq.tryItem total // zero-based!
+
+        head |> should equal (Some total)
+    }
+
+module Other =
+    [<Fact>]
+    let ``TaskSeq-item accepts Int-MaxValue`` () = task {
+        let make50 () = Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
+
+        fun () -> make50 () |> TaskSeq.item Int32.MaxValue |> Task.ignore
+        |> should throwAsyncExact typeof<ArgumentException>
+
+        fun () ->
+            TaskSeq.empty<string>
+            |> TaskSeq.item Int32.MaxValue
+            |> Task.ignore
+        |> should throwAsyncExact typeof<ArgumentException>
+    }
+
+    [<Fact>]
+    let ``TaskSeq-tryItem accepts Int-MaxValue`` () = task {
+        let! nope =
+            Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
+            |> TaskSeq.tryItem Int32.MaxValue
+
+        nope |> should be None'
+
+        let! nope = TaskSeq.empty<string> |> TaskSeq.tryItem Int32.MaxValue
+        nope |> should be None'
+    }
+
+    [<Fact>]
+    let ``TaskSeq-item always throws with negative values`` () = task {
+        let make50 () = Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
+
+        fun () -> make50 () |> TaskSeq.item -1 |> Task.ignore
+        |> should throwAsyncExact typeof<ArgumentException>
+
+        fun () -> make50 () |> TaskSeq.item -10000 |> Task.ignore
+        |> should throwAsyncExact typeof<ArgumentException>
+
+        fun () -> make50 () |> TaskSeq.item Int32.MinValue |> Task.ignore
+        |> should throwAsyncExact typeof<ArgumentException>
+
+        fun () -> TaskSeq.empty<string> |> TaskSeq.item -1 |> Task.ignore
+        |> should throwAsyncExact typeof<ArgumentException>
+
+        fun () -> TaskSeq.empty<string> |> TaskSeq.item -10000 |> Task.ignore
+        |> should throwAsyncExact typeof<ArgumentException>
+
+        fun () ->
+            TaskSeq.empty<string>
+            |> TaskSeq.item Int32.MinValue
+            |> Task.ignore
+        |> should throwAsyncExact typeof<ArgumentException>
+    }
+
+    [<Fact>]
+    let ``TaskSeq-tryItem throws with negative values`` () = task {
+        let make50 () = Gen.sideEffectTaskSeqMicro 50L<µs> 1000L<µs> 50
+
+        let! nothing = make50 () |> TaskSeq.tryItem -1
+        nothing |> should be None'
+
+        let! nothing = make50 () |> TaskSeq.tryItem -10000
+        nothing |> should be None'
+
+        let! nothing = make50 () |> TaskSeq.tryItem Int32.MinValue
+        nothing |> should be None'
+
+        let! nothing = TaskSeq.empty<string> |> TaskSeq.tryItem -1
+        nothing |> should be None'
+
+        let! nothing = TaskSeq.empty<string> |> TaskSeq.tryItem -10000
+        nothing |> should be None'
+
+        let! nothing = TaskSeq.empty<string> |> TaskSeq.tryItem Int32.MinValue
+        nothing |> should be None'
+    }
