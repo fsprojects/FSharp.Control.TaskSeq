d0fc3f43936448d365e13ec977d10531df89625d
Abel Braaksma <abel.online@xs4all.nl>
2022-11-03 03:52:39 +0100

    Expand and improve test data coverage with TaskSeq.zip

--- src/FSharpy.TaskSeq.Test/TaskSeq.Zip.Tests.fs
+++ src/FSharpy.TaskSeq.Test/TaskSeq.Zip.Tests.fs
@@ -11,106 +11,153 @@ open FSharpy
 // TaskSeq.zip
 //
 
-[<Fact>]
-let ``TaskSeq-zip zips in correct order`` () = task {
-    let one = Gen.sideEffectTaskSeq 10
-    let two = Gen.sideEffectTaskSeq 10
-    let combined = TaskSeq.zip one two
-    let! combined = TaskSeq.toArrayAsync combined
+module EmptySeq =
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-zip can zip empty sequences v1`` variant =
+        TaskSeq.zip (Gen.getEmptyVariant variant) (Gen.getEmptyVariant variant)
+        |> verifyEmpty
 
-    combined
-    |> Array.forall (fun (x, y) -> x = y)
-    |> should be True
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-zip can zip empty sequences v2`` variant =
+        TaskSeq.zip TaskSeq.empty<int> (Gen.getEmptyVariant variant)
+        |> verifyEmpty
 
-    combined |> should be (haveLength 10)
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-zip can zip empty sequences v3`` variant =
+        TaskSeq.zip (Gen.getEmptyVariant variant) TaskSeq.empty<int>
+        |> verifyEmpty
 
-    combined
-    |> should equal (Array.init 10 (fun x -> x + 1, x + 1))
-}
 
-[<Fact>]
-let ``TaskSeq-zip zips in correct order for differently delayed sequences`` () = task {
-    let one = Gen.sideEffectTaskSeq_Sequential 10
-    let two = Gen.sideEffectTaskSeq 10
-    let combined = TaskSeq.zip one two
-    let! combined = TaskSeq.toArrayAsync combined
+module Immutable =
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-zip zips in correct order`` variant = task {
+        let one = Gen.getSeqImmutable variant
+        let two = Gen.getSeqImmutable variant
+        let combined = TaskSeq.zip one two
+        let! combined = TaskSeq.toArrayAsync combined
 
-    combined
-    |> Array.forall (fun (x, y) -> x = y)
-    |> should be True
+        combined
+        |> Array.forall (fun (x, y) -> x = y)
+        |> should be True
 
-    combined |> should be (haveLength 10)
+        combined |> should be (haveLength 10)
 
-    combined
-    |> should equal (Array.init 10 (fun x -> x + 1, x + 1))
-}
-
-[<Theory; InlineData 100; InlineData 1_000; InlineData 10_000; InlineData 100_000>]
-let ``TaskSeq-zip zips large sequences just fine`` length = task {
-    let one = Gen.sideEffectTaskSeqMicro 10L<µs> 50L<µs> length
-    let two = Gen.sideEffectTaskSeq_Sequential length
-    let combined = TaskSeq.zip one two
-    let! combined = TaskSeq.toArrayAsync combined
-
-    combined
-    |> Array.forall (fun (x, y) -> x = y)
-    |> should be True
-
-    combined |> should be (haveLength length)
-    combined |> Array.last |> should equal (length, length)
-}
-
-[<Fact>]
-let ``TaskSeq-zip zips different types`` () = task {
-    let one = taskSeq {
-        yield "one"
-        yield "two"
+        combined
+        |> should equal (Array.init 10 (fun x -> x + 1, x + 1))
     }
 
-    let two = taskSeq {
-        yield 42L
-        yield 43L
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-zip zips in correct order for differently delayed sequences`` variant = task {
+        let one = Gen.getSeqImmutable variant
+        let two = taskSeq { yield! [ 1..10 ] }
+        let combined = TaskSeq.zip one two
+        let! combined = TaskSeq.toArrayAsync combined
+
+        combined
+        |> Array.forall (fun (x, y) -> x = y)
+        |> should be True
+
+        combined |> should be (haveLength 10)
+
+        combined
+        |> should equal (Array.init 10 (fun x -> x + 1, x + 1))
     }
 
-    let combined = TaskSeq.zip one two
-    let! combined = TaskSeq.toArrayAsync combined
+module SideEffects =
+    [<Theory; ClassData(typeof<TestSideEffectTaskSeq>)>]
+    let ``TaskSeq-zip zips can deal with side effects in sequences`` variant = task {
+        let one = Gen.getSeqWithSideEffect variant
+        let two = Gen.getSeqWithSideEffect variant
+        let combined = TaskSeq.zip one two
+        let! combined = TaskSeq.toArrayAsync combined
 
-    combined |> should equal [| ("one", 42L); ("two", 43L) |]
-}
+        combined
+        |> Array.forall (fun (x, y) -> x = y)
+        |> should be True
 
-[<Theory; InlineData true; InlineData false>]
-let ``TaskSeq-zip throws on unequal lengths, variant`` leftThrows = task {
-    let long = Gen.sideEffectTaskSeq 11
-    let short = Gen.sideEffectTaskSeq 10
+        combined |> should be (haveLength 10)
 
-    let combined =
-        if leftThrows then
-            TaskSeq.zip short long
-        else
-            TaskSeq.zip long short
+        combined
+        |> should equal (Array.init 10 (fun x -> x + 1, x + 1))
+    }
 
-    fun () -> TaskSeq.toArrayAsync combined |> Task.ignore
-    |> should throwAsyncExact typeof<ArgumentException>
-}
+    [<Theory; ClassData(typeof<TestSideEffectTaskSeq>)>]
+    let ``TaskSeq-zip zips combine a side-effect-free, and a side-effect-full sequence`` variant = task {
+        let one = Gen.getSeqWithSideEffect variant
+        let two = taskSeq { yield! [ 1..10 ] }
+        let combined = TaskSeq.zip one two
+        let! combined = TaskSeq.toArrayAsync combined
 
-[<Theory; InlineData true; InlineData false>]
-let ``TaskSeq-zip throws on unequal lengths with empty seq`` leftThrows = task {
-    let one = Gen.sideEffectTaskSeq 1
+        combined
+        |> Array.forall (fun (x, y) -> x = y)
+        |> should be True
 
-    let combined =
-        if leftThrows then
-            TaskSeq.zip TaskSeq.empty one
-        else
-            TaskSeq.zip one TaskSeq.empty
+        combined |> should be (haveLength 10)
 
-    fun () -> TaskSeq.toArrayAsync combined |> Task.ignore
-    |> should throwAsyncExact typeof<ArgumentException>
-}
+        combined
+        |> should equal (Array.init 10 (fun x -> x + 1, x + 1))
+    }
 
-[<Fact>]
-let ``TaskSeq-zip can zip empty arrays`` () = task {
-    let combined = TaskSeq.zip TaskSeq.empty<int> TaskSeq.empty<string>
-    let! combined = TaskSeq.toArrayAsync combined
-    combined |> should be Empty
-    Array.isEmpty combined |> should be True
-}
+module Performance =
+    [<Theory; InlineData 100; InlineData 1_000; InlineData 10_000; InlineData 100_000>]
+    let ``TaskSeq-zip zips large sequences just fine`` length = task {
+        let one = Gen.sideEffectTaskSeqMicro 10L<µs> 50L<µs> length
+        let two = Gen.sideEffectTaskSeq_Sequential length
+        let combined = TaskSeq.zip one two
+        let! combined = TaskSeq.toArrayAsync combined
+
+        combined
+        |> Array.forall (fun (x, y) -> x = y)
+        |> should be True
+
+        combined |> should be (haveLength length)
+        combined |> Array.last |> should equal (length, length)
+    }
+
+module Other =
+    [<Fact>]
+    let ``TaskSeq-zip zips different types`` () = task {
+        let one = taskSeq {
+            yield "one"
+            yield "two"
+        }
+
+        let two = taskSeq {
+            yield 42L
+            yield 43L
+        }
+
+        let combined = TaskSeq.zip one two
+        let! combined = TaskSeq.toArrayAsync combined
+
+        combined |> should equal [| ("one", 42L); ("two", 43L) |]
+    }
+
+    [<Theory; InlineData true; InlineData false>]
+    let ``TaskSeq-zip throws on unequal lengths, variant`` leftThrows = task {
+        let long = Gen.sideEffectTaskSeq 11
+        let short = Gen.sideEffectTaskSeq 10
+
+        let combined =
+            if leftThrows then
+                TaskSeq.zip short long
+            else
+                TaskSeq.zip long short
+
+        fun () -> TaskSeq.toArrayAsync combined |> Task.ignore
+        |> should throwAsyncExact typeof<ArgumentException>
+    }
+
+    [<Theory; InlineData true; InlineData false>]
+    let ``TaskSeq-zip throws on unequal lengths with empty seq`` leftThrows = task {
+        let one = Gen.sideEffectTaskSeq 1
+
+        let combined =
+            if leftThrows then
+                TaskSeq.zip TaskSeq.empty one
+            else
+                TaskSeq.zip one TaskSeq.empty
+
+        fun () -> TaskSeq.toArrayAsync combined |> Task.ignore
+        |> should throwAsyncExact typeof<ArgumentException>
+    }
