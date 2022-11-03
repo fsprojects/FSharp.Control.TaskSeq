cb5922544d005877a6f9a74b5b4a9631643619d1
Abel Braaksma <abel.online@xs4all.nl>
2022-11-03 02:55:39 +0100

    Add and improve test-data based testing for TaskSeq.map/mapi/mapAsync/mapiAsync

--- src/FSharpy.TaskSeq.Test/TaskSeq.Map.Tests.fs
+++ src/FSharpy.TaskSeq.Test/TaskSeq.Map.Tests.fs
@@ -7,144 +7,218 @@ open FsToolkit.ErrorHandling
 open FSharpy
 
 /// Asserts that a sequence contains the char values 'A'..'J'.
-let validateSequence sequence =
-    sequence
-    |> Seq.map string
-    |> String.concat ""
-    |> should equal "ABCDEFGHIJ"
+let validateSequence ts =
+    ts
+    |> TaskSeq.toSeqCachedAsync
+    |> Task.map (Seq.map string)
+    |> Task.map (String.concat "")
+    |> Task.map (should equal "ABCDEFGHIJ")
 
 /// Validates for "ABCDEFGHIJ" char sequence, or any amount of char-value higher
-let validateSequenceWithOffset offset sequence =
+let validateSequenceWithOffset offset ts =
     let expected =
         [ 'A' .. 'J' ]
         |> List.map (int >> (+) offset >> char >> string)
         |> String.concat ""
 
-    sequence
-    |> Seq.map string
-    |> String.concat ""
-    |> should equal expected
+    ts
+    |> TaskSeq.toSeqCachedAsync
+    |> Task.map (Seq.map string)
+    |> Task.map (String.concat "")
+    |> Task.map (should equal expected)
 
-[<Fact>]
-let ``TaskSeq-map maps in correct order`` () = task {
-    let! sq =
-        Gen.sideEffectTaskSeq 10
+module EmptySeq =
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-map maps in correct order`` variant =
+        Gen.getEmptyVariant variant
         |> TaskSeq.map (fun item -> char (item + 64))
-        |> TaskSeq.toSeqCachedAsync
+        |> verifyEmpty
 
-    validateSequence sq
-}
-
-[<Fact>]
-let ``TaskSeq-mapi maps in correct order`` () = task {
-    let! sq =
-        Gen.sideEffectTaskSeq 10
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-mapi maps in correct order`` variant =
+        Gen.getEmptyVariant variant
         |> TaskSeq.mapi (fun i _ -> char (i + 65))
-        |> TaskSeq.toSeqCachedAsync
+        |> verifyEmpty
 
-    validateSequence sq
-}
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-mapAsync maps in correct order`` variant =
+        Gen.getEmptyVariant variant
+        |> TaskSeq.mapAsync (fun item -> task { return char (item + 64) })
+        |> verifyEmpty
 
-[<Fact>]
-let ``TaskSeq-map can access mutables which are mutated in correct order`` () = task {
-    let mutable sum = 0
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-mapiAsync maps in correct order`` variant =
+        Gen.getEmptyVariant variant
+        |> TaskSeq.mapiAsync (fun i _ -> task { return char (i + 65) })
+        |> verifyEmpty
 
-    let! sq =
-        Gen.sideEffectTaskSeq 10
+
+module Immutable =
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-map maps in correct order`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.map (fun item -> char (item + 64))
+        |> validateSequence
+
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-mapi maps in correct order`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.mapi (fun i _ -> char (i + 65))
+        |> validateSequence
+
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-mapAsync maps in correct order`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.mapAsync (fun item -> task { return char (item + 64) })
+        |> validateSequence
+
+    [<Theory; ClassData(typeof<TestImmTaskSeq>)>]
+    let ``TaskSeq-mapiAsync maps in correct order`` variant =
+        Gen.getSeqImmutable variant
+        |> TaskSeq.mapiAsync (fun i _ -> task { return char (i + 65) })
+        |> validateSequence
+
+module SideEffects =
+    [<Fact>]
+    let ``TaskSeq-map prove that it has no effect until executed`` () =
+        let mutable i = 0
+
+        let ts = taskSeq {
+            i <- i + 1 // we should not get here
+            i <- i + 1
+            yield 42
+            i <- i + 1
+        }
+
+        // point of this test: just calling 'map' won't execute anything of the sequence!
+        let _ =
+            ts
+            |> TaskSeq.map (fun x -> x + 10)
+            |> TaskSeq.map (fun x -> x + 10)
+            |> TaskSeq.map (fun x -> x + 10)
+
+        // multiple maps have no effect unless executed
+        i |> should equal 0
+
+    [<Fact>]
+    let ``TaskSeq-mapi prove that it has no effect until executed`` () =
+        let mutable i = 0
+
+        let ts = taskSeq {
+            i <- i + 1 // we should not get here
+            i <- i + 1
+            yield 42
+            i <- i + 1
+        }
+
+        // point of this test: just calling 'map' won't execute anything of the sequence!
+        let _ =
+            ts
+            |> TaskSeq.mapi (fun x _ -> x + 10)
+            |> TaskSeq.mapi (fun x _ -> x + 10)
+            |> TaskSeq.mapi (fun x _ -> x + 10)
+
+        // multiple maps have no effect unless executed
+        i |> should equal 0
+
+    [<Fact>]
+    let ``TaskSeq-mapAsync prove that it has no effect until executed`` () =
+        let mutable i = 0
+
+        let ts = taskSeq {
+            i <- i + 1 // we should not get here
+            i <- i + 1
+            yield 42
+            i <- i + 1
+        }
+
+        // point of this test: just calling 'map' won't execute anything of the sequence!
+        let _ =
+            ts
+            |> TaskSeq.mapAsync (fun x -> task { return x + 10 })
+            |> TaskSeq.mapAsync (fun x -> task { return x + 10 })
+            |> TaskSeq.mapAsync (fun x -> task { return x + 10 })
+
+        // multiple maps have no effect unless executed
+        i |> should equal 0
+
+    [<Fact>]
+    let ``TaskSeq-mapiAsync prove that it has no effect until executed`` () =
+        let mutable i = 0
+
+        let ts = taskSeq {
+            i <- i + 1 // we should not get here
+            i <- i + 1
+            yield 42
+            i <- i + 1
+        }
+
+        // point of this test: just calling 'map' won't execute anything of the sequence!
+        let _ =
+            ts
+            |> TaskSeq.mapiAsync (fun x _ -> task { return x + 10 })
+            |> TaskSeq.mapiAsync (fun x _ -> task { return x + 10 })
+            |> TaskSeq.mapiAsync (fun x _ -> task { return x + 10 })
+
+        // multiple maps have no effect unless executed
+        i |> should equal 0
+
+
+    [<Theory; ClassData(typeof<TestSideEffectTaskSeq>)>]
+    let ``TaskSeq-map can access mutables which are mutated in correct order`` variant =
+        let mutable sum = 0
+
+        Gen.getSeqWithSideEffect variant
         |> TaskSeq.map (fun item ->
             sum <- sum + 1
             char (sum + 64))
-        |> TaskSeq.toSeqCachedAsync
+        |> validateSequence
+        |> Task.map (fun () -> sum |> should equal 10)
 
-    sum |> should equal 10
-    validateSequence sq
-}
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-mapi can access mutables which are mutated in correct order`` variant =
+        let mutable sum = 0
 
-[<Fact>]
-let ``TaskSeq-mapi can access mutables which are mutated in correct order`` () = task {
-    let mutable sum = 0
-
-    let! sq =
-        Gen.sideEffectTaskSeq 10
+        Gen.getSeqWithSideEffect variant
         |> TaskSeq.mapi (fun i _ ->
             sum <- i + 1
             char (sum + 64))
-        |> TaskSeq.toSeqCachedAsync
+        |> validateSequence
+        |> Task.map (fun () -> sum |> should equal 10)
 
-    sum |> should equal 10
-    validateSequence sq
-}
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-mapAsync can map the same sequence multiple times`` variant = task {
+        let doMap = TaskSeq.mapAsync (fun item -> task { return char (item + 64) })
+        let ts = Gen.getSeqWithSideEffect variant
 
-[<Fact>]
-let ``TaskSeq-mapAsync maps in correct order`` () = task {
-    let! sq =
-        Gen.sideEffectTaskSeq 10
-        |> TaskSeq.mapAsync (fun item -> task { return char (item + 64) })
-        |> TaskSeq.toSeqCachedAsync
+        // each time we do GetAsyncEnumerator(), and go through the whole sequence,
+        // the whole sequence gets re-evaluated, causing our +1 side-effect to run again.
+        do! doMap ts |> validateSequence
+        do! doMap ts |> validateSequenceWithOffset 10 // the mutable is 10 higher
+        do! doMap ts |> validateSequenceWithOffset 20 // again
+        do! doMap ts |> validateSequenceWithOffset 30 // again
+    }
 
-    validateSequence sq
-}
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-mapAsync can access mutables which are mutated in correct order`` variant =
+        let mutable sum = 0
 
-[<Fact>]
-let ``TaskSeq-mapAsync can map the same sequence multiple times`` () = task {
-    let mapAndCache =
-        TaskSeq.mapAsync (fun item -> task { return char (item + 64) })
-        >> TaskSeq.toSeqCachedAsync
-
-    let ts = Gen.sideEffectTaskSeq_Sequential 10
-
-    let! result1 = mapAndCache ts
-    let! result2 = mapAndCache ts
-    let! result3 = mapAndCache ts
-    let! result4 = mapAndCache ts
-    validateSequence result1
-
-    // each time we do GetAsyncEnumerator(), and go through the whole sequence,
-    // the whole sequence gets re-evaluated, causing our +1 side-effect to run again.
-    validateSequenceWithOffset 10 result2 // the mutable is 10 higher
-    validateSequenceWithOffset 20 result3 // again
-    validateSequenceWithOffset 30 result4 // again
-}
-
-[<Fact>]
-let ``TaskSeq-mapiAsync maps in correct order`` () = task {
-    let! sq =
-        Gen.sideEffectTaskSeq 10
-        |> TaskSeq.mapiAsync (fun i _ -> task { return char (i + 65) })
-        |> TaskSeq.toSeqCachedAsync
-
-    validateSequence sq
-}
-
-
-[<Fact>]
-let ``TaskSeq-mapAsync can access mutables which are mutated in correct order`` () = task {
-    let mutable sum = 0
-
-    let! sq =
-        Gen.sideEffectTaskSeq 10
+        Gen.getSeqWithSideEffect variant
         |> TaskSeq.mapAsync (fun item -> task {
             sum <- sum + 1
             return char (sum + 64)
         })
-        |> TaskSeq.toSeqCachedAsync
+        |> validateSequence
+        |> Task.map (fun () -> sum |> should equal 10)
 
-    sum |> should equal 10
-    validateSequence sq
-}
+    [<Theory; ClassData(typeof<TestEmptyVariants>)>]
+    let ``TaskSeq-mapiAsync can access mutables which are mutated in correct order`` variant =
+        let mutable data = '0'
 
-[<Fact>]
-let ``TaskSeq-mapiAsync can access mutables which are mutated in correct order`` () = task {
-    let mutable data = '0'
-
-    let! sq =
-        Gen.sideEffectTaskSeq 10
+        Gen.getSeqWithSideEffect variant
         |> TaskSeq.mapiAsync (fun i _ -> task {
             data <- char (i + 65)
             return data
         })
-        |> TaskSeq.toSeqCachedAsync
-
-    data |> should equal (char 74)
-    validateSequence sq
-}
+        |> validateSequence
+        |> Task.map (fun () -> data |> should equal (char 74))
