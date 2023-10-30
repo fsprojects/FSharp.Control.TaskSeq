f4a4b1142e9eeaccd93a4ed9eeee91ba648d7758
Abel Braaksma <abel.online@xs4all.nl>
2023-10-30 13:48:46 +0100

    Change `module TaskSeq` -> `type TaskSeq` for extension members
    `

--- src/FSharp.Control.TaskSeq/TaskSeq.fs
+++ src/FSharp.Control.TaskSeq/TaskSeq.fs
@@ -6,30 +6,35 @@ open System.Threading.Tasks
 
 #nowarn "57"
 
-module TaskSeq =
+[<AutoOpen>]
+module TaskSeqExtensions =
+    module TaskSeq =
 
-    // Just for convenience
-    module Internal = TaskSeqInternal
+        let empty<'T> =
+            { new IAsyncEnumerable<'T> with
+                member _.GetAsyncEnumerator(_) =
+                    { new IAsyncEnumerator<'T> with
+                        member _.MoveNextAsync() = ValueTask.False
+                        member _.Current = Unchecked.defaultof<'T>
+                        member _.DisposeAsync() = ValueTask.CompletedTask
+                    }
+            }
 
-    let empty<'T> =
-        { new IAsyncEnumerable<'T> with
-            member _.GetAsyncEnumerator(_) =
-                { new IAsyncEnumerator<'T> with
-                    member _.MoveNextAsync() = ValueTask.False
-                    member _.Current = Unchecked.defaultof<'T>
-                    member _.DisposeAsync() = ValueTask.CompletedTask
-                }
-        }
+// Just for convenience
+module Internal = TaskSeqInternal
 
-    let singleton (value: 'T) = Internal.singleton value
+[<Sealed>]
+type TaskSeq =
 
-    let isEmpty source = Internal.isEmpty source
+    static member singleton(value: 'T) = Internal.singleton value
+
+    static member isEmpty source = Internal.isEmpty source
 
     //
     // Convert 'ToXXX' functions
     //
 
-    let toList (source: taskSeq<'T>) = [
+    static member toList(source: taskSeq<'T>) = [
         Internal.checkNonNull (nameof source) source
         let e = source.GetAsyncEnumerator(CancellationToken())
 
@@ -40,7 +45,7 @@ module TaskSeq =
             e.DisposeAsync().AsTask().Wait()
     ]
 
-    let toArray (source: taskSeq<'T>) = [|
+    static member toArray(source: taskSeq<'T>) = [|
         Internal.checkNonNull (nameof source) source
         let e = source.GetAsyncEnumerator(CancellationToken())
 
@@ -51,7 +56,7 @@ module TaskSeq =
             e.DisposeAsync().AsTask().Wait()
     |]
 
-    let toSeq (source: taskSeq<'T>) =
+    static member toSeq(source: taskSeq<'T>) =
         Internal.checkNonNull (nameof source) source
 
         seq {
@@ -65,21 +70,21 @@ module TaskSeq =
                 e.DisposeAsync().AsTask().Wait()
         }
 
-    let toArrayAsync source =
+    static member toArrayAsync source =
         Internal.toResizeArrayAsync source
         |> Task.map (fun a -> a.ToArray())
 
-    let toListAsync source = Internal.toResizeArrayAndMapAsync List.ofSeq source
+    static member toListAsync source = Internal.toResizeArrayAndMapAsync List.ofSeq source
 
-    let toResizeArrayAsync source = Internal.toResizeArrayAsync source
+    static member toResizeArrayAsync source = Internal.toResizeArrayAsync source
 
-    let toIListAsync source = Internal.toResizeArrayAndMapAsync (fun x -> x :> IList<_>) source
+    static member toIListAsync source = Internal.toResizeArrayAndMapAsync (fun x -> x :> IList<_>) source
 
     //
     // Convert 'OfXXX' functions
     //
 
-    let ofArray (source: 'T[]) =
+    static member ofArray(source: 'T[]) =
         Internal.checkNonNull (nameof source) source
 
         taskSeq {
@@ -87,12 +92,12 @@ module TaskSeq =
                 yield c
         }
 
-    let ofList (source: 'T list) = taskSeq {
+    static member ofList(source: 'T list) = taskSeq {
         for c in source do
             yield c
     }
 
-    let ofSeq (source: 'T seq) =
+    static member ofSeq(source: 'T seq) =
         Internal.checkNonNull (nameof source) source
 
         taskSeq {
@@ -100,7 +105,7 @@ module TaskSeq =
                 yield c
         }
 
-    let ofResizeArray (source: 'T ResizeArray) =
+    static member ofResizeArray(source: 'T ResizeArray) =
         Internal.checkNonNull (nameof source) source
 
         taskSeq {
@@ -108,7 +113,7 @@ module TaskSeq =
                 yield c
         }
 
-    let ofTaskSeq (source: #Task<'T> seq) =
+    static member ofTaskSeq(source: #Task<'T> seq) =
         Internal.checkNonNull (nameof source) source
 
         taskSeq {
@@ -117,13 +122,13 @@ module TaskSeq =
                 yield c
         }
 
-    let ofTaskList (source: #Task<'T> list) = taskSeq {
+    static member ofTaskList(source: #Task<'T> list) = taskSeq {
         for c in source do
             let! c = c
             yield c
     }
 
-    let ofTaskArray (source: #Task<'T> array) =
+    static member ofTaskArray(source: #Task<'T> array) =
         Internal.checkNonNull (nameof source) source
 
         taskSeq {
@@ -132,7 +137,7 @@ module TaskSeq =
                 yield c
         }
 
-    let ofAsyncSeq (source: Async<'T> seq) =
+    static member ofAsyncSeq(source: Async<'T> seq) =
         Internal.checkNonNull (nameof source) source
 
         taskSeq {
@@ -141,13 +146,13 @@ module TaskSeq =
                 yield c
         }
 
-    let ofAsyncList (source: Async<'T> list) = taskSeq {
+    static member ofAsyncList(source: Async<'T> list) = taskSeq {
         for c in source do
             let! c = Task.ofAsync c
             yield c
     }
 
-    let ofAsyncArray (source: Async<'T> array) =
+    static member ofAsyncArray(source: Async<'T> array) =
         Internal.checkNonNull (nameof source) source
 
         taskSeq {
@@ -160,21 +165,21 @@ module TaskSeq =
     // Utility functions
     //
 
-    let length source = Internal.lengthBy None source
-    let lengthOrMax max source = Internal.lengthBeforeMax max source
-    let lengthBy predicate source = Internal.lengthBy (Some(Predicate predicate)) source
-    let lengthByAsync predicate source = Internal.lengthBy (Some(PredicateAsync predicate)) source
-    let init count initializer = Internal.init (Some count) (InitAction initializer)
-    let initInfinite initializer = Internal.init None (InitAction initializer)
-    let initAsync count initializer = Internal.init (Some count) (InitActionAsync initializer)
-    let initInfiniteAsync initializer = Internal.init None (InitActionAsync initializer)
+    static member length source = Internal.lengthBy None source
+    static member lengthOrMax max source = Internal.lengthBeforeMax max source
+    static member lengthBy predicate source = Internal.lengthBy (Some(Predicate predicate)) source
+    static member lengthByAsync predicate source = Internal.lengthBy (Some(PredicateAsync predicate)) source
+    static member init count initializer = Internal.init (Some count) (InitAction initializer)
+    static member initInfinite initializer = Internal.init None (InitAction initializer)
+    static member initAsync count initializer = Internal.init (Some count) (InitActionAsync initializer)
+    static member initInfiniteAsync initializer = Internal.init None (InitActionAsync initializer)
 
-    let delay (generator: unit -> taskSeq<'T>) =
+    static member delay(generator: unit -> taskSeq<'T>) =
         { new IAsyncEnumerable<'T> with
             member _.GetAsyncEnumerator(ct) = generator().GetAsyncEnumerator(ct)
         }
 
-    let concat (sources: taskSeq<#taskSeq<'T>>) =
+    static member concat(sources: taskSeq<#taskSeq<'T>>) =
         Internal.checkNonNull (nameof sources) sources
 
         taskSeq {
@@ -183,7 +188,7 @@ module TaskSeq =
                 yield! (ts :> taskSeq<'T>)
         }
 
-    let append (source1: taskSeq<'T>) (source2: taskSeq<'T>) =
+    static member append (source1: taskSeq<'T>) (source2: taskSeq<'T>) =
         Internal.checkNonNull (nameof source1) source1
         Internal.checkNonNull (nameof source2) source2
 
@@ -192,7 +197,7 @@ module TaskSeq =
             yield! source2
         }
 
-    let appendSeq (source1: taskSeq<'T>) (source2: seq<'T>) =
+    static member appendSeq (source1: taskSeq<'T>) (source2: seq<'T>) =
         Internal.checkNonNull (nameof source1) source1
         Internal.checkNonNull (nameof source2) source2
 
@@ -201,7 +206,7 @@ module TaskSeq =
             yield! source2
         }
 
-    let prependSeq (source1: seq<'T>) (source2: taskSeq<'T>) =
+    static member prependSeq (source1: seq<'T>) (source2: taskSeq<'T>) =
         Internal.checkNonNull (nameof source1) source1
         Internal.checkNonNull (nameof source2) source2
 
@@ -214,60 +219,60 @@ module TaskSeq =
     // iter/map/collect functions
     //
 
-    let cast source : taskSeq<'T> = Internal.map (SimpleAction(fun (x: obj) -> x :?> 'T)) source
-    let box source = Internal.map (SimpleAction box) source
-    let unbox<'U when 'U: struct> (source: taskSeq<obj>) : taskSeq<'U> = Internal.map (SimpleAction unbox) source
-    let iter action source = Internal.iter (SimpleAction action) source
-    let iteri action source = Internal.iter (CountableAction action) source
-    let iterAsync action source = Internal.iter (AsyncSimpleAction action) source
-    let iteriAsync action source = Internal.iter (AsyncCountableAction action) source
-    let map (mapper: 'T -> 'U) source = Internal.map (SimpleAction mapper) source
-    let mapi (mapper: int -> 'T -> 'U) source = Internal.map (CountableAction mapper) source
-    let mapAsync mapper source = Internal.map (AsyncSimpleAction mapper) source
-    let mapiAsync mapper source = Internal.map (AsyncCountableAction mapper) source
-    let collect (binder: 'T -> #taskSeq<'U>) source = Internal.collect binder source
-    let collectSeq (binder: 'T -> #seq<'U>) source = Internal.collectSeq binder source
-    let collectAsync (binder: 'T -> #Task<#taskSeq<'U>>) source : taskSeq<'U> = Internal.collectAsync binder source
-    let collectSeqAsync (binder: 'T -> #Task<#seq<'U>>) source : taskSeq<'U> = Internal.collectSeqAsync binder source
+    static member cast source : taskSeq<'T> = Internal.map (SimpleAction(fun (x: obj) -> x :?> 'T)) source
+    static member box source = Internal.map (SimpleAction box) source
+    static member unbox<'U when 'U: struct>(source: taskSeq<obj>) : taskSeq<'U> = Internal.map (SimpleAction unbox) source
+    static member iter action source = Internal.iter (SimpleAction action) source
+    static member iteri action source = Internal.iter (CountableAction action) source
+    static member iterAsync action source = Internal.iter (AsyncSimpleAction action) source
+    static member iteriAsync action source = Internal.iter (AsyncCountableAction action) source
+    static member map (mapper: 'T -> 'U) source = Internal.map (SimpleAction mapper) source
+    static member mapi (mapper: int -> 'T -> 'U) source = Internal.map (CountableAction mapper) source
+    static member mapAsync mapper source = Internal.map (AsyncSimpleAction mapper) source
+    static member mapiAsync mapper source = Internal.map (AsyncCountableAction mapper) source
+    static member collect (binder: 'T -> #taskSeq<'U>) source = Internal.collect binder source
+    static member collectSeq (binder: 'T -> #seq<'U>) source = Internal.collectSeq binder source
+    static member collectAsync (binder: 'T -> #Task<#taskSeq<'U>>) source : taskSeq<'U> = Internal.collectAsync binder source
+    static member collectSeqAsync (binder: 'T -> #Task<#seq<'U>>) source : taskSeq<'U> = Internal.collectSeqAsync binder source
 
     //
     // choosers, pickers and the like
     //
 
-    let tryHead source = Internal.tryHead source
+    static member tryHead source = Internal.tryHead source
 
-    let head source =
+    static member head source =
         Internal.tryHead source
         |> Task.map (Option.defaultWith Internal.raiseEmptySeq)
 
-    let tryLast source = Internal.tryLast source
+    static member tryLast source = Internal.tryLast source
 
-    let last source =
+    static member last source =
         Internal.tryLast source
         |> Task.map (Option.defaultWith Internal.raiseEmptySeq)
 
-    let tryTail source = Internal.tryTail source
+    static member tryTail source = Internal.tryTail source
 
-    let tail source =
+    static member tail source =
         Internal.tryTail source
         |> Task.map (Option.defaultWith Internal.raiseEmptySeq)
 
-    let tryItem index source = Internal.tryItem index source
+    static member tryItem index source = Internal.tryItem index source
 
-    let item index source =
+    static member item index source =
         if index < 0 then
             invalidArg (nameof index) "The input must be non-negative."
 
         Internal.tryItem index source
         |> Task.map (Option.defaultWith Internal.raiseInsufficient)
 
-    let tryExactlyOne source = Internal.tryExactlyOne source
+    static member tryExactlyOne source = Internal.tryExactlyOne source
 
-    let exactlyOne source =
+    static member exactlyOne source =
         Internal.tryExactlyOne source
         |> Task.map (Option.defaultWith (fun () -> invalidArg (nameof source) "The input sequence contains more than one element."))
 
-    let indexed (source: taskSeq<'T>) =
+    static member indexed(source: taskSeq<'T>) =
         Internal.checkNonNull (nameof source) source
 
         taskSeq {
@@ -278,56 +283,56 @@ module TaskSeq =
                 i <- i + 1
         }
 
-    let choose chooser source = Internal.choose (TryPick chooser) source
-    let chooseAsync chooser source = Internal.choose (TryPickAsync chooser) source
-    let filter predicate source = Internal.filter (Predicate predicate) source
-    let filterAsync predicate source = Internal.filter (PredicateAsync predicate) source
-    let takeWhile predicate source = Internal.takeWhile Exclusive (Predicate predicate) source
-    let takeWhileAsync predicate source = Internal.takeWhile Exclusive (PredicateAsync predicate) source
-    let takeWhileInclusive predicate source = Internal.takeWhile Inclusive (Predicate predicate) source
-    let takeWhileInclusiveAsync predicate source = Internal.takeWhile Inclusive (PredicateAsync predicate) source
-    let tryPick chooser source = Internal.tryPick (TryPick chooser) source
-    let tryPickAsync chooser source = Internal.tryPick (TryPickAsync chooser) source
-    let tryFind predicate source = Internal.tryFind (Predicate predicate) source
-    let tryFindAsync predicate source = Internal.tryFind (PredicateAsync predicate) source
-    let tryFindIndex predicate source = Internal.tryFindIndex (Predicate predicate) source
-    let tryFindIndexAsync predicate source = Internal.tryFindIndex (PredicateAsync predicate) source
-    let except itemsToExclude source = Internal.except itemsToExclude source
-    let exceptOfSeq itemsToExclude source = Internal.exceptOfSeq itemsToExclude source
+    static member choose chooser source = Internal.choose (TryPick chooser) source
+    static member chooseAsync chooser source = Internal.choose (TryPickAsync chooser) source
+    static member filter predicate source = Internal.filter (Predicate predicate) source
+    static member filterAsync predicate source = Internal.filter (PredicateAsync predicate) source
+    static member takeWhile predicate source = Internal.takeWhile Exclusive (Predicate predicate) source
+    static member takeWhileAsync predicate source = Internal.takeWhile Exclusive (PredicateAsync predicate) source
+    static member takeWhileInclusive predicate source = Internal.takeWhile Inclusive (Predicate predicate) source
+    static member takeWhileInclusiveAsync predicate source = Internal.takeWhile Inclusive (PredicateAsync predicate) source
+    static member tryPick chooser source = Internal.tryPick (TryPick chooser) source
+    static member tryPickAsync chooser source = Internal.tryPick (TryPickAsync chooser) source
+    static member tryFind predicate source = Internal.tryFind (Predicate predicate) source
+    static member tryFindAsync predicate source = Internal.tryFind (PredicateAsync predicate) source
+    static member tryFindIndex predicate source = Internal.tryFindIndex (Predicate predicate) source
+    static member tryFindIndexAsync predicate source = Internal.tryFindIndex (PredicateAsync predicate) source
+    static member except itemsToExclude source = Internal.except itemsToExclude source
+    static member exceptOfSeq itemsToExclude source = Internal.exceptOfSeq itemsToExclude source
 
-    let exists predicate source =
+    static member exists predicate source =
         Internal.tryFind (Predicate predicate) source
         |> Task.map (Option.isSome)
 
-    let existsAsync predicate source =
+    static member existsAsync predicate source =
         Internal.tryFind (PredicateAsync predicate) source
         |> Task.map (Option.isSome)
 
-    let contains value source =
+    static member contains value source =
         Internal.tryFind (Predicate((=) value)) source
         |> Task.map (Option.isSome)
 
-    let pick chooser source =
+    static member pick chooser source =
         Internal.tryPick (TryPick chooser) source
         |> Task.map (Option.defaultWith Internal.raiseNotFound)
 
-    let pickAsync chooser source =
+    static member pickAsync chooser source =
         Internal.tryPick (TryPickAsync chooser) source
         |> Task.map (Option.defaultWith Internal.raiseNotFound)
 
-    let find predicate source =
+    static member find predicate source =
         Internal.tryFind (Predicate predicate) source
         |> Task.map (Option.defaultWith Internal.raiseNotFound)
 
-    let findAsync predicate source =
+    static member findAsync predicate source =
         Internal.tryFind (PredicateAsync predicate) source
         |> Task.map (Option.defaultWith Internal.raiseNotFound)
 
-    let findIndex predicate source =
+    static member findIndex predicate source =
         Internal.tryFindIndex (Predicate predicate) source
         |> Task.map (Option.defaultWith Internal.raiseNotFound)
 
-    let findIndexAsync predicate source =
+    static member findIndexAsync predicate source =
         Internal.tryFindIndex (PredicateAsync predicate) source
         |> Task.map (Option.defaultWith Internal.raiseNotFound)
 
@@ -337,8 +342,6 @@ module TaskSeq =
     // zip/unzip/fold etc functions
     //
 
-    let zip source1 source2 = Internal.zip source1 source2
-
-    let fold folder state source = Internal.fold (FolderAction folder) state source
-
-    let foldAsync folder state source = Internal.fold (AsyncFolderAction folder) state source
+    static member zip source1 source2 = Internal.zip source1 source2
+    static member fold folder state source = Internal.fold (FolderAction folder) state source
+    static member foldAsync folder state source = Internal.fold (AsyncFolderAction folder) state source
